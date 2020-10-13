96c42be6944c1f039acc9df60f71ad1346a593fc
LBHBomotoso <46450498+LBHBomotoso@users.noreply.github.com>
2020-10-13 16:53:56 +0100

    updatre entity

--- SocialCareCaseViewerApi/V1/Domain/Entity.cs
+++ SocialCareCaseViewerApi/V1/Domain/Entity.cs
@@ -5,7 +5,7 @@ namespace SocialCareCaseViewerApi.V1.Domain
     //TODO: rename this class to be the domain object which this API will getting. e.g. Residents or Claimants
     public class Entity
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
         public DateTime CreatedAt { get; set; }
 
         //TODO: Add fields which you are interested in
--- SocialCareCaseViewerApi/V1/Factories/EntityFactory.cs
+++ SocialCareCaseViewerApi/V1/Factories/EntityFactory.cs
@@ -5,15 +5,15 @@ namespace SocialCareCaseViewerApi.V1.Factories
 {
     public static class EntityFactory
     {
-        public static Entity ToDomain(this DatabaseEntity databaseEntity)
+        public static Entity ToDomain(this Person databaseEntity)
         {
             //TODO: Map the rest of the fields in the domain object.
             // More information on this can be found here https://github.com/LBHackney-IT/lbh-base-api/wiki/Factory-object-mappings
 
             return new Entity
             {
-                Id = databaseEntity.Id,
-                CreatedAt = databaseEntity.CreatedAt,
+                Id = databaseEntity.Id
+                //CreatedAt = databaseEntity.CreatedAt,
             };
         }
     }
