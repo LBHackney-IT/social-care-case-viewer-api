17b6fc5f50c7e03c338cc301529a3b8294b56a67
LBHBomotoso <46450498+LBHBomotoso@users.noreply.github.com>
2020-10-16 17:51:17 +0100

    Update ListCasesRequest.cs

--- SocialCareCaseViewerApi/V1/Boundary/Requests/ListCasesRequest.cs
+++ SocialCareCaseViewerApi/V1/Boundary/Requests/ListCasesRequest.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,10 @@ namespace SocialCareCaseViewerApi.V1.Boundary.Requests
 {
     public class ListCasesRequest
     {
+        [FromQuery(Name = "mosaic_id")]
         public string MosaicId { get; set; }
+
+        [FromQuery(Name = "worker_email")]
         public string WorkerEmail { get; set; }
     }
 }
