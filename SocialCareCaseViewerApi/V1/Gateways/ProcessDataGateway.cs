3948466778eda7282e904f657937014ceb131f2e
LBHBomotoso <46450498+LBHBomotoso@users.noreply.github.com>
2020-10-20 17:05:22 +0100

    perform regular expression search on doc db

--- SocialCareCaseViewerApi/V1/Gateways/ProcessDataGateway.cs
+++ SocialCareCaseViewerApi/V1/Gateways/ProcessDataGateway.cs
@@ -37,7 +37,7 @@ namespace SocialCareCaseViewerApi.V1.Gateways
             }
             var filter = !string.IsNullOrEmpty(request.WorkerEmail) ?
                 Builders<BsonDocument>.Filter.Eq("worker_email", request.WorkerEmail)
-                : Builders<BsonDocument>.Filter.Eq("mosaic_id", mosaicId);
+                : Builders<BsonDocument>.Filter.Regex("mosaic_id", new BsonRegularExpression(mosaicId.ToString()));
 
             var result = _sccvDbContext.getCollection().Find(filter).ToList();
             //if document does not exist in the DB, then thrown a corresponsing error.
