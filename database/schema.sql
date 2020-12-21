1d7154858b14351725b99813c4e20ef8fffc134a
LBHTKarki <58432863+LBHTKarki@users.noreply.github.com>
2020-12-21 14:33:11 +0000

    add new person import script

--- database/schema.sql
+++ database/schema.sql
@@ -208,5 +208,63 @@ create table dbo.SCCV_ALLOCATIONS
     LAST_NAME                   varchar(30) not null,
     FIRST_NAME                  varchar(30) not null,
     WORKER_EMAIL                varchar(62),
-	LAC varchar(10)
+	LAC 						varchar(10)
 );
+
+--import new persons script
+do
+$$
+        declare
+                new_person record;
+                new_person_mosaic_id bigint;
+        begin
+        --TODO: truncate table for full import
+                for new_person
+                in SELECT
+                        imports.first_name,
+                        imports.last_name,
+                        imports.full_name,
+                        imports.date_of_birth,
+                        imports.person_id,
+                        imports.gender,
+                        imports.restricted,
+                        imports.context_flag
+
+        FROM
+                dbo.SCCV_PERSONS_LOOKUP as lookup RIGHT OUTER JOIN
+                dbo.SCCV_PERSONS_IMPORT as imports ON lookup.NC_ID = imports.PERSON_ID
+        WHERE lookup.PERSON_ID is null AND LOWER(imports.PERSON_ID) like 'nc%'
+
+        loop
+                new_person_mosaic_id = null;
+
+                insert into dbo.dm_persons(
+                        first_name,
+                        last_name,
+                        full_name,
+                        date_of_birth,
+                        gender,
+                        restricted,
+                        context_flag,
+                        from_dm_person
+                )
+                values (
+                        new_person.first_name,
+                        new_person.last_name,
+                        new_person.full_name,
+                        new_person.date_of_birth,
+                        new_person.gender,
+                        new_person.restricted,
+                        new_person.context_flag,
+                        'N'
+                )
+                returning person_id into new_person_mosaic_id;
+
+                insert into dbo.sccv_persons_lookup(person_id, nc_id)
+                        values (new_person_mosaic_id, new_person.person_id);
+
+                raise notice '% - %', new_person.person_id, new_person_mosaic_id;
+        end loop;
+end;
+$$
+
