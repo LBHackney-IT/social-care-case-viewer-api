cb121c2091b1d8d81e6afe38a063ce935af407c0
LBHTKarki <58432863+LBHTKarki@users.noreply.github.com>
2020-12-22 11:35:15 +0000

    resolve cursor issue

--- SocialCareCaseViewerApi/V1/Boundary/Response/CareCaseDataList.cs
+++ SocialCareCaseViewerApi/V1/Boundary/Response/CareCaseDataList.cs
@@ -5,6 +5,6 @@ namespace SocialCareCaseViewerApi.V1.Boundary.Response
     public class CareCaseDataList
     {
         public List<CareCaseData> Cases { get; set; }
-        public string NextCursor { get; set; }
+        public int? NextCursor { get; set; }
     }
 }
