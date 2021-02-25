6d98c94c9d65cceef1c5e1873d12240d4f5c9af1
LBHTKarki <58432863+LBHTKarki@users.noreply.github.com>
2021-02-25 16:23:05 +0000

    Hotfix/search cases by both nc and mosaic ids (#129)
    * return records for both mosaic and nc references
    
    * fix formatting
    
    * fix code formatting config

--- SocialCareCaseViewerApi/V1/Gateways/IProcessDataGateway.cs
+++ SocialCareCaseViewerApi/V1/Gateways/IProcessDataGateway.cs
@@ -8,7 +8,7 @@ namespace SocialCareCaseViewerApi.V1.Gateways
 {
     public interface IProcessDataGateway
     {
-        Tuple<IEnumerable<CareCaseData>, int> GetProcessData(ListCasesRequest request);
+        Tuple<IEnumerable<CareCaseData>, int> GetProcessData(ListCasesRequest request, string ncId);
         Task<string> InsertCaseNoteDocument(CaseNotesDocument caseNotesDoc);
     }
 }
