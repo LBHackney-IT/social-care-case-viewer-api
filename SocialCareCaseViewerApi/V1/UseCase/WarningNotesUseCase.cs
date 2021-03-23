2bf5e6e63a478807e4ff7898fcd6a5e7278bda61
Ben Reynolds-Carr <ben.reynolds-carr@hackney.gov.uk>
2021-03-23 15:52:40 +0000

    Hotfix: WarningNotesUseCase added to startup scope

--- SocialCareCaseViewerApi/V1/UseCase/WarningNotesUseCase.cs
+++ SocialCareCaseViewerApi/V1/UseCase/WarningNotesUseCase.cs
@@ -1,10 +1,11 @@
 using SocialCareCaseViewerApi.V1.Boundary.Requests;
 using SocialCareCaseViewerApi.V1.Boundary.Response;
 using SocialCareCaseViewerApi.V1.Gateways;
+using SocialCareCaseViewerApi.V1.UseCase.Interfaces;
 
 namespace SocialCareCaseViewerApi.V1.UseCase
 {
-    public class WarningNotesUseCase
+    public class WarningNotesUseCase : IWarningNotesUseCase
     {
         private IDatabaseGateway _databaseGateway;
         public WarningNotesUseCase(IDatabaseGateway databaseGateway)
