e650991b0c62692b394aab5a7bd0300cca7dcc30
JohnFarrellDev <johnfarrell@fastmail.com>
2021-04-15 15:20:05 +0100

    log get cases error

--- SocialCareCaseViewerApi/V1/Controllers/SocialCareCaseViewerApiController.cs
+++ SocialCareCaseViewerApi/V1/Controllers/SocialCareCaseViewerApiController.cs
@@ -109,6 +109,7 @@ namespace SocialCareCaseViewerApi.V1.Controllers
         /// <response code="200">Success. Returns cases related to the specified ID or officer email</response>
         /// <response code="400">One or more dates are invalid or missing</response>
         /// <response code="404">No cases found for the specified ID or officer email</response>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>")]
         [ProducesResponseType(typeof(CareCaseDataList), StatusCodes.Status200OK)]
         [HttpGet]
         [Route("cases")]
@@ -118,14 +119,18 @@ namespace SocialCareCaseViewerApi.V1.Controllers
             {
                 string dateValidationError = null;
 
-                if (!string.IsNullOrWhiteSpace(request.StartDate) && !DateTime.TryParseExact(request.StartDate, "dd-MM-yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime startDate))
+                if (!string.IsNullOrWhiteSpace(request.StartDate) && !DateTime.TryParseExact(request.StartDate,
+                    "dd-MM-yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime startDate))
                 {
                     dateValidationError += "Invalid start date";
                 }
-                if (!string.IsNullOrWhiteSpace(request.EndDate) && !DateTime.TryParseExact(request.EndDate, "dd-MM-yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime endDate))
+
+                if (!string.IsNullOrWhiteSpace(request.EndDate) && !DateTime.TryParseExact(request.EndDate,
+                    "dd-MM-yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime endDate))
                 {
                     dateValidationError += " Invalid end date";
                 }
+
                 if (!string.IsNullOrEmpty(dateValidationError))
                 {
                     return StatusCode(400, dateValidationError);
@@ -136,6 +141,10 @@ namespace SocialCareCaseViewerApi.V1.Controllers
             catch (DocumentNotFoundException e)
             {
                 return NotFound(e.Message);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
             }
         }
 
