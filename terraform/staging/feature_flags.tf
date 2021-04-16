e8e747bcc52484dcb1c638f973713a479dae64a1
JohnFarrellDev <johnfarrell@fastmail.com>
2021-04-16 16:36:10 +0100

    remove local terraform env variables

--- terraform/staging/feature_flags.tf
+++ terraform/staging/feature_flags.tf
@@ -6,12 +6,3 @@ resource "aws_secretsmanager_secret_version" "show_historic_data_feature_flag" {
   secret_id     = aws_secretsmanager_secret.show_historic_data_feature_flag.id
   secret_string = "true"
 }
-
-resource "aws_secretsmanager_secret" "deallocation_date_feature_flag" {
-    name = "social_care_case_viewer_api_deallocation_date"
-}
-
-resource "aws_secretsmanager_secret_version" "social_care_case_viewer_api_deallocation_date" {
-    secret_id     = aws_secretsmanager_secret.deallocation_date_feature_flag.id
-    secret_string = "true"
-}
