03eb69d84a7dd1679670a6acedbeecdc6ae898a3
JohnFarrellDev <johnfarrell@fastmail.com>
2021-04-16 14:14:30 +0100

    make rule validation check feature flags

--- SocialCareCaseViewerApi/V1/Boundary/Requests/UpdateAllocationRequest.cs
+++ SocialCareCaseViewerApi/V1/Boundary/Requests/UpdateAllocationRequest.cs
@@ -18,6 +18,8 @@ namespace SocialCareCaseViewerApi.V1.Boundary.Requests
     {
         public UpdateAllocationRequestValidator()
         {
+            var requireDeallocationDate = Environment.GetEnvironmentVariable("SOCIAL_CARE_DEALLOCATION_DATE") is ("true");
+
             RuleFor(x => x.Id)
                 .NotNull().WithMessage("Id Required")
                 .InclusiveBetween(1, int.MaxValue).WithMessage("Id must be grater than 1");
@@ -27,8 +29,8 @@ namespace SocialCareCaseViewerApi.V1.Boundary.Requests
                 .NotNull().WithMessage("Email required")
                 .EmailAddress().WithMessage("Provide a valid email");
             RuleFor(x => x.DeallocationDate)
-                .NotNull().WithMessage("Deallocation date required")
-                .LessThan(DateTime.Now).WithMessage("Deallocation date must be in the past");
+                .NotNull().WithMessage("Deallocation date required").When(_ => requireDeallocationDate)
+                .LessThan(DateTime.Now).WithMessage("Deallocation date must be in the past").When(_ => requireDeallocationDate);
         }
     }
 }
