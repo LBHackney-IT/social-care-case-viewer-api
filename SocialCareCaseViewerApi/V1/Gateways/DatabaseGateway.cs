e3a4bcc8ec76eaff2049730856f0389f7f822dea
JohnFarrellDev <johnfarrell@fastmail.com>
2021-04-19 19:22:31 +0100

    make allocation date for creating of assignment required

--- SocialCareCaseViewerApi/V1/Gateways/DatabaseGateway.cs
+++ SocialCareCaseViewerApi/V1/Gateways/DatabaseGateway.cs
@@ -562,10 +562,10 @@ namespace SocialCareCaseViewerApi.V1.Gateways
 
         #endregion
 
-        private static AllocationSet SetDeallocationValues(AllocationSet allocation, DateTime? dt, string modifiedBy)
+        private static AllocationSet SetDeallocationValues(AllocationSet allocation, DateTime dt, string modifiedBy)
         {
             //keep workerId and TeamId in the record so they can be easily exposed to front end
-            allocation.AllocationEndDate = dt ?? DateTime.Now;
+            allocation.AllocationEndDate = dt;
             allocation.CaseStatus = "Closed";
             allocation.CaseClosureDate = dt;
             allocation.LastModifiedBy = modifiedBy;
