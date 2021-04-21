92462572672917696a455dd0a0ca8284b873b6ff
JohnFarrellDev <johnfarrell@fastmail.com>
2021-04-21 18:03:55 +0100

    create worker controller with POST endpoint for creating worker, validate request body

---
+++ SocialCareCaseViewerApi/V1/Controllers/WorkersController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SocialCareCaseViewerApi.V1.Boundary.Requests;
+
+namespace SocialCareCaseViewerApi.V1.Controllers
+{
+    [ApiController]
+    //TODO: Rename to match the APIs endpoint
+    [Route("api/v1/workers")]
+    [Produces("application/json")]
+    [ApiVersion("1.0")]
+    public class WorkersController : BaseController
+    {
+        public WorkersController()
+        {
+
+        }
+
+        /// <summary>
+        /// Create a worker
+        /// </summary>
+        /// <param name="request"></param>
+        /// <response code="200">Worker created successfully</response>
+        /// <response code="400">Invalid CreateWorkerRequest received</response>
+        /// <response code="404">Exception encountered</response>
+        /// <response code="500">There was a problem updating the record</response>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpPost]
+        public IActionResult CreateWorker([FromBody] CreateWorkerRequest request)
+        {
+            var validator = new CreateWorkerRequestValidator();
+            var validationResults = validator.Validate(request);
+
+            if (!validationResults.IsValid)
+            {
+                return BadRequest(validationResults.ToString());
+            }
+
+            return Ok("place holder");
+        }
+
+    }
+}
