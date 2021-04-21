650cee34c42e7e0140b976b0c7457c027ada9e66
JohnFarrellDev <johnfarrell@fastmail.com>
2021-04-21 18:53:17 +0100

    implement nullable into infrastructure worker

--- SocialCareCaseViewerApi/V1/Infrastructure/Worker.cs
+++ SocialCareCaseViewerApi/V1/Infrastructure/Worker.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
+#nullable enable
 namespace SocialCareCaseViewerApi.V1.Infrastructure
 {
     [Table("sccv_worker", Schema = "dbo")]
@@ -15,26 +16,26 @@ namespace SocialCareCaseViewerApi.V1.Infrastructure
         [Column("email")]
         [MaxLength(62)]
         [Required]
-        public string Email { get; set; }
+        public string Email { get; set; } = null!;
 
         [Column("first_name")]
         [MaxLength(100)]
         [Required]
-        public string FirstName { get; set; }
+        public string FirstName { get; set; } = null!;
 
         [Column("last_name")]
         [MaxLength(100)]
         [Required]
-        public string LastName { get; set; }
+        public string LastName { get; set; } = null!;
 
         [Column("role")]
         [MaxLength(200)]
-        public string Role { get; set; }
+        public string Role { get; set; } = null!;
 
 
         //nav props
-        public ICollection<WorkerTeam> WorkerTeams { get; set; }
+        public ICollection<WorkerTeam> WorkerTeams { get; set; } = null!;
 
-        public ICollection<AllocationSet> Allocations { get; set; }
+        public ICollection<AllocationSet> Allocations { get; set; }  = null!;
     }
 }
