cd96af8bc2d3b320a5e6f6285bb749199577a027
JohnFarrellDev <johnfarrell@fastmail.com>
2021-04-21 21:06:40 +0100

    update request and validation to meet expected JSON body

--- SocialCareCaseViewerApi/V1/Boundary/Requests/CreateWorkerRequest.cs
+++ SocialCareCaseViewerApi/V1/Boundary/Requests/CreateWorkerRequest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
 using FluentValidation;
 
@@ -7,7 +8,7 @@ namespace SocialCareCaseViewerApi.V1.Boundary.Requests
 {
     public class CreateWorkerRequest
     {
-        [JsonPropertyName("emailAddress")]
+        [JsonPropertyName("email")]
         public string EmailAddress { get; set; } = null!;
 
         [JsonPropertyName("firstName")]
@@ -16,17 +17,29 @@ namespace SocialCareCaseViewerApi.V1.Boundary.Requests
         [JsonPropertyName("lastName")]
         public string LastName { get; set; } = null!;
 
+        [JsonPropertyName("createdBy")]
+        public string CreatedBy { get; set; } = null!;
+
         [JsonPropertyName("contextFlag")]
         public string ContextFlag { get; set; } = null!;
 
-        [JsonPropertyName("team")]
-        public string Team { get; set; } = null!;
+        [JsonPropertyName("teams")]
+        public List<WorkerTeamRequest> Teams { get; set; } = null!;
 
         [JsonPropertyName("role")]
         public string Role { get; set; } = null!;
 
         [JsonPropertyName("dateStart")]
         public DateTime DateStart { get; set; }
+    }
+
+    public class WorkerTeamRequest
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = null!;
     }
 
     public class CreateWorkerRequestValidator : AbstractValidator<CreateWorkerRequest>
@@ -49,17 +62,27 @@ namespace SocialCareCaseViewerApi.V1.Boundary.Requests
             RuleFor(w => w.ContextFlag)
                 .NotNull().WithMessage("Context flag must be provided")
                 .MinimumLength(1).WithMessage("Context flag must be provided")
-                .MaximumLength(100).WithMessage("Context flag must be no longer than 100 characters");
-            RuleFor(w => w.Team)
-                .NotNull().WithMessage("Team must be provided")
-                .MinimumLength(1).WithMessage("Team must be provided")
-                .MaximumLength(200).WithMessage("Team provided is too long (more than 200 characters)");
+                .MaximumLength(1).WithMessage("Context flag must be no longer than 1 character");
+            RuleFor(w => w.Teams.Count).GreaterThan(0).WithMessage("A team must be provided");
+            RuleForEach(w => w.Teams)
+                .ChildRules(team =>
+                {
+                    team.RuleFor(t => t.Id).NotNull().WithMessage("Teams must be provided with an ID");
+                    team.RuleFor(t => t.Id).GreaterThan(0).WithMessage("Team ID must be greater than 0");
+                    team.RuleFor(t => t.Name).NotNull().WithMessage("Team must be provided with a name");
+                    team.RuleFor(t => t.Name).MinimumLength(1).WithMessage("Team must be provided with a name");
+                    team.RuleFor(t => t.Name).MaximumLength(200).WithMessage("Team name must be no more than 100 characters");
+                });
             RuleFor(w => w.Role)
                 .NotNull().WithMessage("Role must be provided")
                 .MinimumLength(1).WithMessage("Role must be provided")
                 .MaximumLength(200).WithMessage("Role provided is too long (more than 200 characters)");
             RuleFor(w => w.DateStart)
                 .NotNull().WithMessage("Start date must be provided");
+            RuleFor(w => w.CreatedBy)
+                .NotNull().WithMessage("Created by email address must be provided")
+                .MaximumLength(62).WithMessage("Created by email address must be no longer than 62 characters")
+                .EmailAddress().WithMessage("Created by email address must be valid");
         }
     }
 
