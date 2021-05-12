bf63ef3c86661b396c6d21e282853dfcc3064c78
JohnFarrellDev <johnkevinfarrell93@gmail.com>
2021-05-12 10:42:41 +0100

    Update properties on GetTeamsRequest query and validation

--- SocialCareCaseViewerApi/V1/Boundary/Requests/GetTeamsRequest.cs
+++ SocialCareCaseViewerApi/V1/Boundary/Requests/GetTeamsRequest.cs
@@ -6,6 +6,12 @@ namespace SocialCareCaseViewerApi.V1.Boundary.Requests
 {
     public class GetTeamsRequest
     {
+        [FromQuery(Name = "id")]
+        public int? Id { get; set; }
+
+        [FromQuery(Name = "name")]
+        public string? Name { get; set; }
+
         [FromQuery(Name = "context_flag")]
         public string? ContextFlag { get; set; }
     }
@@ -14,6 +20,11 @@ namespace SocialCareCaseViewerApi.V1.Boundary.Requests
     {
         public GetTeamsRequestValidator()
         {
+            RuleFor(t => t).Must(t => t.Name != null || t.ContextFlag != null || t.Id != null)
+                .WithMessage("Must provide either a team ID, team name or context flag");
+            RuleFor(t => t.Name)
+                .MinimumLength(1).WithMessage("Team name must be at least 1 character")
+                .MaximumLength(200).WithMessage("Team name has a maximum length of 200 characters");
             RuleFor(t => t.ContextFlag)
                 .NotNull().WithMessage("Context flag must be provided")
                 .MinimumLength(1).WithMessage("Context flag must be provided")
