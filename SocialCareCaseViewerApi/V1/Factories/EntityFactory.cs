9ddf3d91acf13daa803afd6ddcff45de2583d591
JohnFarrellDev <johnkevinfarrell93@gmail.com>
2021-05-13 17:52:53 +0100

    update entity factory todomain to supply team context to worker teams

--- SocialCareCaseViewerApi/V1/Factories/EntityFactory.cs
+++ SocialCareCaseViewerApi/V1/Factories/EntityFactory.cs
@@ -75,7 +75,7 @@ namespace SocialCareCaseViewerApi.V1.Factories
                 CreatedBy = worker.CreatedBy,
                 DateStart = worker.DateStart,
                 AllocationCount = worker?.Allocations == null ? 0 : worker.Allocations.Where(x => x.CaseStatus.ToUpper() == "OPEN").Count(),
-                Teams = includeTeamData ? worker.WorkerTeams?.Select(x => new Team() { Id = x.Team.Id, Name = x.Team.Name }).ToList() : null,
+                Teams = includeTeamData ? worker.WorkerTeams?.Select(x => new Team() { Id = x.Team.Id, Name = x.Team.Name, Context = x.Team.Context}).ToList() : null,
             };
         }
 
