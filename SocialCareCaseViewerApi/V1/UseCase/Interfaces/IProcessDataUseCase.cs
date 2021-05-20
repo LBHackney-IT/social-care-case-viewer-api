ac286a3c66ba08dcc7eb360c9245051de734d58d
JohnFarrellDev <johnkevinfarrell93@gmail.com>
2021-05-20 18:11:13 +0100

    explicit null handling

--- SocialCareCaseViewerApi/V1/UseCase/Interfaces/IProcessDataUseCase.cs
+++ SocialCareCaseViewerApi/V1/UseCase/Interfaces/IProcessDataUseCase.cs
@@ -2,11 +2,12 @@ using System.Threading.Tasks;
 using SocialCareCaseViewerApi.V1.Boundary.Requests;
 using SocialCareCaseViewerApi.V1.Boundary.Response;
 
-namespace SocialCareCaseViewerApi.V1.UseCase
+#nullable enable
+namespace SocialCareCaseViewerApi.V1.UseCase.Interfaces
 {
     public interface IProcessDataUseCase
     {
-        CareCaseDataList Execute(ListCasesRequest request);
+        CareCaseDataList Execute(ListCasesRequest? request);
 
         CareCaseData Execute(string recordId);
         Task<string> Execute(CreateCaseNoteRequest request);
