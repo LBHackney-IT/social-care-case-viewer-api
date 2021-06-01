2181db93774ff152bb89bcf4a3807492f5f7b530
JohnFarrellDev <johnfarrell@fastmail.com>
2021-06-01 16:01:18 +0100

    create object to represent stored case submission

---
+++ SocialCareCaseViewerApi/V1/Infrastructure/CaseSubmission.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SocialCareCaseViewerApi.V1.Infrastructure
+
+#nullable enable
+{
+    public class CaseSubmission
+    {
+        [BsonId]
+        public Guid FormId { get; set; }
+
+        public string CreatedBy { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; }
+
+        public List<Person> Residents { get; set; } = null!;
+
+        public List<Worker> Workers { get; set; } = null!;
+
+        public List<(Worker, DateTime)> EditHistory { get; set; } = null!;
+
+        public DateTime? IsEditable { get; set; }
+
+        public DateTime? IsSubmitted { get; set; }
+
+        // outer hashset int represents step id for form, inner hashset int represents questionId, answer value stored as string
+        public Dictionary<int, Dictionary<int, string>> FormAnswers { get; set; } = null!;
+    }
+}
