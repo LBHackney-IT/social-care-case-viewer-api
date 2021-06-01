c939d0408988738ca7a46271bab22c595cdb168b
JohnFarrellDev <johnfarrell@fastmail.com>
2021-06-01 16:29:06 +0100

    create use case for inserting a case submission record to database

--- SocialCareCaseViewerApi/V1/UseCase/SubmissionsUseCase.cs
+++ SocialCareCaseViewerApi/V1/UseCase/SubmissionsUseCase.cs
@@ -1,14 +1,21 @@
+using System;
+using System.Collections.Generic;
 using SocialCareCaseViewerApi.V1.Boundary.Requests;
 using SocialCareCaseViewerApi.V1.Boundary.Response;
+using SocialCareCaseViewerApi.V1.Exceptions;
+using SocialCareCaseViewerApi.V1.Factories;
 using SocialCareCaseViewerApi.V1.Gateways;
+using SocialCareCaseViewerApi.V1.Infrastructure;
 using SocialCareCaseViewerApi.V1.UseCase.Interfaces;
 
+#nullable enable
 namespace SocialCareCaseViewerApi.V1.UseCase
 {
     public class SubmissionsUseCase : ISubmissionsUseCase
     {
         private readonly IDatabaseGateway _databaseGateway;
         private readonly IMongoGateway _mongoGateway;
+        private const string CollectionName = "resident-case-submissions";
 
         public SubmissionsUseCase(IDatabaseGateway databaseGateway, IMongoGateway mongoGateway)
         {
@@ -18,7 +25,36 @@ namespace SocialCareCaseViewerApi.V1.UseCase
 
         public CaseSubmissionResponse ExecutePost(CreateCaseSubmissionRequest request)
         {
-            throw new System.NotImplementedException();
+            var worker = _databaseGateway.GetWorkerByEmail(request.CreatedBy);
+            if (worker == null)
+            {
+                throw new WorkerNotFoundException($"Worker with email {request.CreatedBy} not found");
+            }
+
+            var resident = _databaseGateway.GetPersonByMosaicId(request.ResidentId);
+            if (resident == null)
+            {
+                throw new PersonNotFoundException($"Person with id {request.ResidentId} not found");
+            }
+
+            var dateTimeNow = DateTime.Now;
+
+            var caseSubmission = new CaseSubmission
+            {
+                FormId = new Guid(),
+                Residents = new List<Person> {resident},
+                Workers = new List<Worker> {worker},
+                CreatedAt = dateTimeNow,
+                CreatedBy = worker,
+                IsEditable = dateTimeNow,
+                IsSubmitted = null,
+                EditHistory = new List<(Worker, DateTime)> {(worker, dateTimeNow)},
+                FormAnswers = new Dictionary<int, Dictionary<int, string>>()
+            };
+
+            _mongoGateway.InsertRecord(CollectionName, caseSubmission);
+
+            return caseSubmission.ToDomain().ToResponse();
         }
     }
 }
