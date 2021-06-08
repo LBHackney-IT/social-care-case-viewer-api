abe4497469eb221b6d5e3248fa8410d46abfe1ef
JohnFarrellDev <johnfarrell@fastmail.com>
2021-06-08 16:31:54 +0100

    add validator to startup

--- SocialCareCaseViewerApi/Startup.cs
+++ SocialCareCaseViewerApi/Startup.cs
@@ -125,6 +125,8 @@ namespace SocialCareCaseViewerApi
             services.AddTransient<IValidator<CreateTeamRequest>, CreateTeamRequestValidator>();
             services.AddTransient<IValidator<GetTeamsRequest>, GetTeamsRequestValidator>();
             services.AddTransient<IValidator<CreateCaseSubmissionRequest>, CreateCaseSubmissionRequestValidator>();
+            services
+                .AddTransient<IValidator<UpdateFormSubmissionAnswersRequest>, UpdateFormSubmissionAnswersValidator>();
         }
 
         private static void ConfigureDbContext(IServiceCollection services)
