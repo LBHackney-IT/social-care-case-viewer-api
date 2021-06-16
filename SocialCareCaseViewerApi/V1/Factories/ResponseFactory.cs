8bb0559b422631e8f6c9f5e7ea27a7ed52c415d1
JohnFarrellDev <johnfarrell@fastmail.com>
2021-06-16 18:03:05 +0100

    make response factory nullable and handle SubmittedBy possibly being null

--- SocialCareCaseViewerApi/V1/Factories/ResponseFactory.cs
+++ SocialCareCaseViewerApi/V1/Factories/ResponseFactory.cs
@@ -13,6 +13,7 @@ using dbPhoneNumber = SocialCareCaseViewerApi.V1.Infrastructure.PhoneNumber;
 using Team = SocialCareCaseViewerApi.V1.Domain.Team;
 using WarningNote = SocialCareCaseViewerApi.V1.Domain.WarningNote;
 
+#nullable enable
 namespace SocialCareCaseViewerApi.V1.Factories
 {
     public static class ResponseFactory
@@ -112,8 +113,6 @@ namespace SocialCareCaseViewerApi.V1.Factories
 
         public static WorkerResponse ToResponse(this Domain.Worker worker)
         {
-            if (worker == null) return null;
-
             return new WorkerResponse
             {
                 Id = worker.Id,
@@ -141,7 +140,7 @@ namespace SocialCareCaseViewerApi.V1.Factories
         public static GetPersonResponse ToResponse(Person person)
         {
             //get the current display address
-            dbAddress displayAddress = person?.Addresses?.FirstOrDefault(x => x.IsDisplayAddress?.ToUpper() == "Y");
+            var displayAddress = person.Addresses?.FirstOrDefault(x => x.IsDisplayAddress?.ToUpper() == "Y");
 
             return new GetPersonResponse()
             {
@@ -163,8 +162,8 @@ namespace SocialCareCaseViewerApi.V1.Factories
                 Restricted = person.Restricted,
                 Title = person.Title,
                 Address = displayAddress != null ? EntityFactory.DbAddressToAddressDomain(displayAddress) : null,
-                OtherNames = person.OtherNames?.Select(x => x.ToDomain())?.ToList(),
-                PhoneNumbers = person.PhoneNumbers?.Select(x => x.ToDomain())?.ToList()
+                OtherNames = person.OtherNames?.Select(x => x.ToDomain()).ToList(),
+                PhoneNumbers = person.PhoneNumbers?.Select(x => x.ToDomain()).ToList()
             };
         }
 
@@ -252,7 +251,7 @@ namespace SocialCareCaseViewerApi.V1.Factories
                 CreatedAt = caseSubmission.CreatedAt,
                 CreatedBy = caseSubmission.CreatedBy.ToResponse(),
                 SubmittedAt = caseSubmission.SubmittedAt,
-                SubmittedBy = caseSubmission.SubmittedBy.ToResponse(),
+                SubmittedBy = caseSubmission.SubmittedBy?.ToResponse(),
                     EditHistory = caseSubmission.EditHistory.Select(e => new EditHistory<WorkerResponse>
                 {
                     EditTime = e.EditTime,
