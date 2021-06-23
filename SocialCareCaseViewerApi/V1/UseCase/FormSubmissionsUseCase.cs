4a2f153abe886f9a93f41b8177385f2b9283416a
Miles-Alford <43789512+Miles-Alford@users.noreply.github.com>
2021-06-23 16:28:39 +0100

    Bugfix: add loop to remove circular reference for allocation worker (#322)
    * add function to remove circular reference
    
    * Linting
    
    Co-authored-by: ana <anabebic.x@gmail.com>

--- SocialCareCaseViewerApi/V1/UseCase/FormSubmissionsUseCase.cs
+++ SocialCareCaseViewerApi/V1/UseCase/FormSubmissionsUseCase.cs
@@ -59,6 +59,11 @@ namespace SocialCareCaseViewerApi.V1.UseCase
                 name.Person = null;
             }
 
+            for (var index = 0; index < resident.Allocations?.Count; index++)
+            {
+                var allocation = resident.Allocations[index];
+                allocation.Person = null;
+            }
 
             var dateTimeNow = DateTime.Now;
 
