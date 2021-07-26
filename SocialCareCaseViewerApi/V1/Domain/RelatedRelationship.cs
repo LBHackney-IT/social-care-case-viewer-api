848b1d792a0903b78025b8cf23eb3c88cfcafdf9
John Farrell <johnfarrell@fastmail.com>
2021-07-26 11:32:22 +0100

    Development (#387)
    * SCT-833 - Add a new temporary object to the BE response
    
    * Fix formatting
    
    * Add createdBy to create personal relationship request
    
    * Fix not auditing personal relationship and details
    
    * Nullable details
    
    * Formatting
    
    * Formatting (again)
    
    * Formatting (newline)
    
    * Update PersonalRelationship#ExecutePost to check if createdBy email exists
    
    * Update create personal relationship endpoint to 400 if createdBy doesn't exist
    
    * Add email address check for createdBy in CreatePersonalRelationshipRequest
    
    * SCT-811 soft delete recovery script (#371)
    
    * SCT-641 enable search by emergency ids (#364)
    
    * SCT-856 add support for 0 prefixes in emergency ids in person search (#372)
    
    * add leading zero handling
    
    * use correct var in test
    
    * add form name overall field and update staging bucket name (#374)
    
    * SCT-479 - Create "Delete Relationship" Service API Endpoint
    
    * SCT-479 - Added null check for relationship details
    
    * SCT-479 - Fixed inverse type get
    
    * SCT-479 - More tests and refactors
    
    * Added tests for DeleteRelationships + SavePersonWithPersonalRelationshipAndOppositeToDatabase helper function
    
    * Updated relationship names (unique key)
    
    * Update SocialCareCaseViewerApi.Tests/V1/Controllers/Relationship/RelationshipControllerDeletePersonalRelationshipTests.cs
    
    Co-authored-by: Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
    
    * Relationship fix
    
    * Fix tests
    
    * Better tests
    
    * Reverted schema.sql
    
    * Refactor deleting relationship to cascade on delete
    
    This takes advantage of navigational properties set up which means
    when we delete the `PersonalRelationship` it will also delete
    `PersonalRelationshipDetail` because we've included it when searching.
    
    * add simple tracking for get person and get case endpoints
    
    * format
    
    * format
    
    * improve test coverage
    
    * format
    
    * Update SocialCareCaseViewerApi.Tests/V1/Controllers/CaseControllerTests.cs
    
    Co-authored-by: Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
    
    * improve tests
    
    * format
    
    * add manual database update script
    
    * Remove `persons` object from get relationships response
    
    This has been replaced with `relationships` instead as we updated the
    response object but left this to enable the frontend to switch over.
    
    * temp to figure out get worker error
    
    * print stack trace instead of message
    
    * return stack trace
    
    * move throwing exception into db gateway
    
    * temp remove teams, allocations, worker teams
    
    * include team data with worker
    
    * remove the debug exception handling
    
    * fix: use null forgiving operator (#383)
    
    * feat(SCT-902): Panel approvals for submissions (#379)
    
    * add new test cases for panel approval
    
    * add PanelApproved to usecase
    
    * add panel approvedAt and approvedBy properties
    
    * write use case tests for new submission state, fix caught bugs
    
    * formatting
    
    * use panel_approved for consistency
    
    * update submission state supplied in test
    
    * update rules so only created by can not approve the submission
    
    * update exception text in test
    
    * Create 2021-07-21_1-update_is_active_flag.md
    
    Add documentation for manual update
    
    * Replace ID with name of attribute
    
    * add missing content
    
    Co-authored-by: mapleleafjack <jack.musajo@madetech.com>
    Co-authored-by: Wen Ting Wang <wenting@madetech.com>
    Co-authored-by: Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
    Co-authored-by: LBHTKarki <58432863+LBHTKarki@users.noreply.github.com>
    Co-authored-by: Tuomo Karki <tuomo.karki@hackney.gov.uk>
    Co-authored-by: Renny Omorinsola Fadoju <32823756+Renny4Real@users.noreply.github.com>
    Co-authored-by: Marta Pederiva <86296201+martapederiva@users.noreply.github.com>

--- SocialCareCaseViewerApi/V1/Domain/RelatedRelationship.cs
+++ SocialCareCaseViewerApi/V1/Domain/RelatedRelationship.cs
@@ -6,11 +6,11 @@ namespace SocialCareCaseViewerApi.V1.Domain
     {
         public long Id { get; set; }
         public long PersonId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Gender { get; set; }
-        public string IsMainCarer { get; set; }
-        public string IsInformalCarer { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Gender { get; set; } = null!;
+        public string IsMainCarer { get; set; } = null!;
+        public string IsInformalCarer { get; set; } = null!;
         public string? Details { get; set; }
     }
 }
