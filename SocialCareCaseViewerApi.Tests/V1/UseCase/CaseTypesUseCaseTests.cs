77850137bc9588c9e6c835d36389e1d6ef50916c
mapleleafjack <jack.musajo@madetech.com>
2021-08-11 11:28:18 +0100

    Case Statuses test improvements (part 2)

--- SocialCareCaseViewerApi.Tests/V1/UseCase/CaseTypesUseCaseTests.cs
+++ SocialCareCaseViewerApi.Tests/V1/UseCase/CaseTypesUseCaseTests.cs
@@ -34,5 +34,38 @@ namespace SocialCareCaseViewerApi.Tests.V1.UseCase
                 .WithMessage("Person not found");
 
         }
+
+
+        [Test]
+        public void WhenPersonIsFoundWithNoCaseStatusesReturnsEmptyList()
+        {
+            var person = TestHelpers.CreatePerson();
+            var emptyResponse = new List<CaseStatus>() { };
+            
+            _mockDatabaseGateway.Setup(x => x.GetPersonByMosaicId(person.Id)).Returns(person);
+            _mockDatabaseGateway.Setup(x => x.GetCaseStatusesByPersonId(person.Id)).Returns(emptyResponse);
+
+            var result = _caseStatusesUseCase.ExecuteGet(person.Id);
+
+            result.Should().BeEquivalentTo(new ListCaseStatusesResponse() { PersonId = person.Id, CaseStatuses = new List<SocialCareCaseViewerApi.V1.Domain.CaseStatus>(){} });
+        }
+        
+        [Test]
+        public void WhenPersonIsFoundWithCaseStatusesReturnsCaseStatusesList()
+        {
+            var caseStatusType = TestHelpers.CreateCaseStatusType();
+            var caseStatusSubtype = TestHelpers.CreateCaseStatusSubtype(typeId: caseStatusType.Id);
+            var person = TestHelpers.CreatePerson();
+            var csus = TestHelpers.CreateCaseStatus(personId: person.Id, typeId: caseStatusType.Id, subtypeId: caseStatusSubtype.Id, startDate: DateTime.Now, notes: "Testing");
+
+            var response = new List<CaseStatus>() { csus };
+            
+            _mockDatabaseGateway.Setup(x => x.GetPersonByMosaicId(person.Id)).Returns(person);
+            _mockDatabaseGateway.Setup(x => x.GetCaseStatusesByPersonId(person.Id)).Returns(response);
+
+            var result = _caseStatusesUseCase.ExecuteGet(person.Id);
+
+            result.CaseStatuses.Count.Should().Be(1);
+        }
     }
 }
