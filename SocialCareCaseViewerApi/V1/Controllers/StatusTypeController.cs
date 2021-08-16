075f6d3aca8f456f52205bfbd4e1befe9bd10684
mapleleafjack <jack.musajo@madetech.com>
2021-08-16 12:35:52 +0100

    slash to ?

--- SocialCareCaseViewerApi/V1/Controllers/StatusTypeController.cs
+++ SocialCareCaseViewerApi/V1/Controllers/StatusTypeController.cs
@@ -29,7 +29,7 @@ namespace SocialCareCaseViewerApi.V1.Controllers
         /// <response code="404">Case status not found</response>
         [ProducesResponseType(typeof(ListRelationshipsResponse), StatusCodes.Status200OK)]
         [HttpGet]
-        [Route("residents/{personId:long}/casestatuses/{endDate:string}")]
+        [Route("residents/{personId:long}/casestatuses?{endDate:string}")]
         public IActionResult ListCaseStatuses(long personId, string endDate)
         {
             try
