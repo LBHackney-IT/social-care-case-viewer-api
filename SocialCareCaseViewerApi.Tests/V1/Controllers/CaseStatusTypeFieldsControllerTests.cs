805ede6f662e3d70943c73c42326833661b9adc2
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2021-08-19 16:44:19 +0100

    Use fixture to generate test response

--- SocialCareCaseViewerApi.Tests/V1/Controllers/CaseStatusTypeFieldsControllerTests.cs
+++ SocialCareCaseViewerApi.Tests/V1/Controllers/CaseStatusTypeFieldsControllerTests.cs
@@ -1,18 +1,13 @@
-using System.Collections.Generic;
+using AutoFixture;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
 using SocialCareCaseViewerApi.V1.Controllers;
-using SocialCareCaseViewerApi.V1.Factories;
 using SocialCareCaseViewerApi.V1.UseCase.Interfaces;
 using SocialCareCaseViewerApi.V1.Boundary.Response;
-using SocialCareCaseViewerApi.Tests.V1.Gateways;
 using SocialCareCaseViewerApi.V1.Boundary.Requests;
-using SocialCareCaseViewerApi.V1.Infrastructure;
 using SocialCareCaseViewerApi.V1.UseCase;
-using CaseStatusTypeField = SocialCareCaseViewerApi.V1.Domain.CaseStatusTypeField;
-using CaseStatusTypeFieldOption = SocialCareCaseViewerApi.V1.Domain.CaseStatusTypeFieldOption;
 
 namespace SocialCareCaseViewerApi.Tests.V1.Controllers
 {
@@ -21,7 +16,7 @@ namespace SocialCareCaseViewerApi.Tests.V1.Controllers
     {
         private CaseStatusTypeFieldsController _caseStatusTypeFieldsController;
         private Mock<IGetCaseStatusFieldsUseCase> _mockCaseStatusesUseCase;
-        // private readonly Fixture _fixture = new Fixture();
+        private readonly Fixture _fixture = new Fixture();
 
         [SetUp]
         public void SetUp()
@@ -33,43 +28,14 @@ namespace SocialCareCaseViewerApi.Tests.V1.Controllers
         [Test]
         public void GetCaseStatusTypeFieldsByTypeReturns200WhenCaseStatusTypeIsFound()
         {
-            CaseStatusType caseStatusType = DatabaseGatewayTests.GetValidCaseStatusTypeWithFields("Test");
-
+            var getCaseStatusFieldsResponse = _fixture.Create<GetCaseStatusFieldsResponse>();
             _mockCaseStatusesUseCase.Setup(x => x.Execute(It.IsAny<GetCaseStatusFieldsRequest>()))
-                .Returns(new GetCaseStatusFieldsResponse()
-                {
-                    Name = caseStatusType.Name,
-                    Description = caseStatusType.Description,
-                    Fields = caseStatusType.Fields.ToResponse()
-                });
+                .Returns(getCaseStatusFieldsResponse);
 
             var response = _caseStatusTypeFieldsController.GetCaseStatusTypeFields("Test") as ObjectResult;
 
             response?.StatusCode.Should().Be(200);
-            response?.Value.Should().BeEquivalentTo(new GetCaseStatusFieldsResponse
-            {
-                Description = "Test Type",
-                Name = "Test",
-                Fields = new List<CaseStatusTypeField>
-                {
-                    new CaseStatusTypeField
-                    {
-                        Description = "Something",
-                        Name = "someThing",
-                        Options = new List<CaseStatusTypeFieldOption>
-                        {
-                            new CaseStatusTypeFieldOption
-                            {
-                                Description = "The first option", Name = "One"
-                            },
-                            new CaseStatusTypeFieldOption
-                            {
-                                Description = "The second option", Name = "Two"
-                            }
-                        }
-                    }
-                }
-            });
+            response?.Value.Should().BeEquivalentTo(getCaseStatusFieldsResponse);
         }
 
         [Test]
