8ccae51b0713769b8c7774a6a568aec15671882e
Marta Pederiva <86296201+martapederiva@users.noreply.github.com>
2021-08-24 11:59:45 +0100

    added handling of PersonID as a filter

--- SocialCareCaseViewerApi.Tests/V1/Boundary/Request/QueryCaseSubmissionsRequestTests.cs
+++ SocialCareCaseViewerApi.Tests/V1/Boundary/Request/QueryCaseSubmissionsRequestTests.cs
@@ -18,7 +18,8 @@ namespace SocialCareCaseViewerApi.Tests.V1.Boundary.Request
             new object?[] { TestHelpers.CreateQueryCaseSubmissions(submissionStates: new List<string>{"submission-state"}), true, null},
             new object?[] { TestHelpers.CreateQueryCaseSubmissions(createdBefore: DateTime.Now), true, null},
             new object?[] { TestHelpers.CreateQueryCaseSubmissions(createdAfter: DateTime.Now), true, null},
-            new object?[] { TestHelpers.CreateQueryCaseSubmissions(workerEmail: "example@hackney.gov.uk"), true, null}
+            new object?[] { TestHelpers.CreateQueryCaseSubmissions(workerEmail: "example@hackney.gov.uk"), true, null},
+            new object?[] { TestHelpers.CreateQueryCaseSubmissions(personID: 2), true, null}
         };
 
         [TestCaseSource(nameof(_queryCaseSubmissionRequests))]
