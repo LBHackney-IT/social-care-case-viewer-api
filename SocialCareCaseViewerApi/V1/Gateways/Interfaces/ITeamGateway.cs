bf77255e388b8bed7be8386529bf2171a46a6719
JohnFarrellDev <johnfarrell@fastmail.com>
2021-09-25 23:42:18 +0100

    create team gateway with functionality for creating a team

---
+++ SocialCareCaseViewerApi/V1/Gateways/Interfaces/ITeamGateway.cs
@@ -0,0 +1,11 @@
+using SocialCareCaseViewerApi.V1.Boundary.Requests;
+using SocialCareCaseViewerApi.V1.Domain;
+
+#nullable enable
+namespace SocialCareCaseViewerApi.V1.Gateways.Interfaces
+{
+    public interface ITeamGateway
+    {
+        Team CreateTeam(CreateTeamRequest request);
+    }
+}
