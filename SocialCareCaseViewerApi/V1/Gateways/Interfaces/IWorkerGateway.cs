951372311b0583b68863a22ab01c8c748699a4fd
JohnFarrellDev <johnfarrell@fastmail.com>
2021-09-27 21:32:43 +0100

    use of nullable

--- SocialCareCaseViewerApi/V1/Gateways/Interfaces/IWorkerGateway.cs
+++ SocialCareCaseViewerApi/V1/Gateways/Interfaces/IWorkerGateway.cs
@@ -1,9 +1,10 @@
 using SocialCareCaseViewerApi.V1.Domain;
 
+#nullable enable
 namespace SocialCareCaseViewerApi.V1.Gateways.Interfaces
 {
     public interface IWorkerGateway
     {
-        Worker GetWorkerByWorkerId(int workerId);
+        Worker? GetWorkerByWorkerId(int workerId);
     }
 }
