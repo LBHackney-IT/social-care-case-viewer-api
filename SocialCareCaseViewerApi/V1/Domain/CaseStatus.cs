a45fb115185913677afc6a41aa4a6b630edc26d0
JohnFarrellDev <johnfarrell@fastmail.com>
2021-09-27 16:30:24 +0100

    update domain types, should be using datetimes to represent datetime and not strings

--- SocialCareCaseViewerApi/V1/Domain/CaseStatus.cs
+++ SocialCareCaseViewerApi/V1/Domain/CaseStatus.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 #nullable enable
@@ -6,10 +7,10 @@ namespace SocialCareCaseViewerApi.V1.Domain
     public class CaseStatus
     {
         public long Id { get; set; }
-        public string? Type { get; set; }
-        public string? StartDate { get; set; }
-        public string? EndDate { get; set; }
+        public string Type { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
         public string? Notes { get; set; }
-        public List<CaseStatusField> Fields { get; set; } = new List<CaseStatusField>();
+        public List<CaseStatusField> Fields { get; set; }
     }
 }
