220ae4322f13416c760b3b87bae94ae5a4786d04
JohnFarrellDev <johnfarrell@fastmail.com>
2021-10-25 14:45:43 +0100

    add required properties to infrastructure class for our mash referrals

--- SocialCareCaseViewerApi/V1/Infrastructure/MashReferral.cs
+++ SocialCareCaseViewerApi/V1/Infrastructure/MashReferral.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using Newtonsoft.Json;
@@ -10,5 +13,25 @@ namespace SocialCareCaseViewerApi.V1.Infrastructure
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public ObjectId Id { get; set; }
+
+        public string Referrer { get; set; }
+
+        public string RequestedSupport { get; set; }
+
+        public Worker AssignedTo { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public IEnumerable<string> Clients { get; set; }
+
+        public string ReferralDocumentURI { get; set; }
+
+        public string Stage { get; set }
+
+        public string InitialDecision { get; set; }
+        public string ScreeningDecision { get; set; }
+        public string FinalDecision { get; set; }
+
+        public string ReferralCategory { get; set; }
     }
 }
