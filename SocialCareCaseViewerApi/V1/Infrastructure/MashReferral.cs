cd54312863ae95632f76b57b7d82fd43410c4097
JohnFarrellDev <johnfarrell@fastmail.com>
2021-10-25 13:58:17 +0100

    add id property to mash referral

--- SocialCareCaseViewerApi/V1/Infrastructure/MashReferral.cs
+++ SocialCareCaseViewerApi/V1/Infrastructure/MashReferral.cs
@@ -1,7 +1,14 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+
 namespace SocialCareCaseViewerApi.V1.Infrastructure
 {
     public class MashReferral
     {
-        
+        [JsonProperty("_id")]
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
     }
 }
