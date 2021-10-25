6f0418ec30b2b2dcd4e5abc90eee7893bcd56311
JohnFarrellDev <johnfarrell@fastmail.com>
2021-10-25 14:46:19 +0100

    for reset usecase have a variety of mash referrals

--- SocialCareCaseViewerApi/V1/UseCase/MashReferralUseCase.cs
+++ SocialCareCaseViewerApi/V1/UseCase/MashReferralUseCase.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using SocialCareCaseViewerApi.V1.Gateways;
 using SocialCareCaseViewerApi.V1.Gateways.Interfaces;
 using SocialCareCaseViewerApi.V1.Infrastructure;
@@ -18,17 +20,137 @@ namespace SocialCareCaseViewerApi.V1.UseCase
         {
             _mashReferralGateway.Reset();
 
-            var referral1 = new MashReferral { };
-            var referral2 = new MashReferral { };
-            var referral3 = new MashReferral { };
-            var referral4 = new MashReferral { };
-            var referral5 = new MashReferral { };
+            var referral1 = new MashReferral
+            {
+                Referrer = "Police - red",
+                CreatedAt = DateTime.Now.AddHours(-3),
+                RequestedSupport = "Safeguarding",
+                Clients = new List<string> {"Sally Samuels"},
+                Stage = "Contact",
+                ReferralDocumentURI = "hardcoded-referral-1-URI"
+            };
+            var referral2 = new MashReferral
+            {
+                Referrer = "School",
+                CreatedAt = DateTime.Now.AddHours(-5),
+                RequestedSupport = "Safeguarding",
+                Clients = new List<string> {"Bert Bertram", "c2", "c3", "c4"},
+                Stage = "Contact",
+                ReferralDocumentURI = "hardcoded-referral-2-URI"
+            };
+            var referral3 = new MashReferral
+            {
+                Referrer = "Family",
+                CreatedAt = DateTime.Now.AddHours(-10),
+                RequestedSupport = "Safeguarding",
+                Clients = new List<string> {"Bertie Stephens", "c2", "c3", "c4"},
+                Stage = "Contact",
+                ReferralDocumentURI = "hardcoded-referral-3-URI"
+            };
+            var referral4 = new MashReferral
+            {
+                Referrer = "Individual",
+                CreatedAt = DateTime.Now.AddHours(-9),
+                RequestedSupport = "Early help",
+                Clients = new List<string> { "Elysia Hughs" },
+                Stage = "Contact",
+                ReferralDocumentURI = "hardcoded-referral-4-URI"
+            };
+            var referral5 = new MashReferral
+            {
+                Referrer = "School",
+                CreatedAt = DateTime.Now.AddHours(-2),
+                RequestedSupport = "Early help",
+                Clients = new List<string> { "Benj Stephens" },
+                Stage = "Contact",
+                ReferralDocumentURI = "hardcoded-referral-5-URI"
+            };
+            var referral6 = new MashReferral
+            {
+                Referrer = "Police - green",
+                CreatedAt = DateTime.Now.AddHours(-2),
+                RequestedSupport = "Early help",
+                Clients = new List<string> { "Sophie Smith" },
+                Stage = "Contact",
+                ReferralDocumentURI = "hardcoded-referral-6-URI"
+            };
+            var referral7 = new MashReferral
+            {
+                Referrer = "Police - red",
+                CreatedAt = DateTime.Now.AddHours(-3),
+                RequestedSupport = "Safeguarding",
+                Clients = new List<string> { "Sally Stephens" },
+                Stage = "Initial decision",
+                ReferralDocumentURI = "hardcoded-referral-7-URI"
+            };
+            var referral8 = new MashReferral
+            {
+                Referrer = "Police - green",
+                CreatedAt = DateTime.Now.AddHours(-1),
+                RequestedSupport = "Safeguarding",
+                Clients = new List<string> { "Barry Smith", "c1", "c2" },
+                Stage = "Contact",
+                ReferralDocumentURI = "hardcoded-referral-8-URI"
+            };
+            var referral9 = new MashReferral
+            {
+                Referrer = "Police - red",
+                CreatedAt = DateTime.Now.AddHours(-3),
+                RequestedSupport = "Safeguarding",
+                Clients = new List<string> { "Sophie Owens" },
+                Stage = "Screening",
+                ReferralDocumentURI = "hardcoded-referral-9-URI",
+                InitialDecision = "DAIS",
+                ReferralCategory = "Emotional abuse"
+            };
+            var referral10 = new MashReferral
+            {
+                Referrer = "Police - green",
+                CreatedAt = DateTime.Now.AddHours(-1),
+                RequestedSupport = "Safeguarding",
+                Clients = new List<string> { "Max Smith", "c1", "c2" },
+                Stage = "Screening",
+                ReferralDocumentURI = "hardcoded-referral-10-URI",
+                InitialDecision = "DAIS",
+                ReferralCategory = "Emotional abuse"
+            };
+            var referral11 = new MashReferral
+            {
+                Referrer = "Police - red",
+                CreatedAt = DateTime.Now.AddHours(-3),
+                RequestedSupport = "Safeguarding",
+                Clients = new List<string> { "John Smith" },
+                Stage = "Screening",
+                ReferralDocumentURI = "hardcoded-referral-11-URI",
+                InitialDecision = "DAIS",
+                ReferralCategory = "Emotional abuse",
+                ScreeningDecision = "DAIS"
+            };
+            var referral12 = new MashReferral
+            {
+                Referrer = "Police - green",
+                CreatedAt = DateTime.Now.AddHours(-1),
+                RequestedSupport = "Safeguarding",
+                Clients = new List<string> { "Jack Owens", "c1", "c2" },
+                Stage = "Screening",
+                ReferralDocumentURI = "hardcoded-referral-12-URI",
+                InitialDecision = "DAIS",
+                ReferralCategory = "Emotional abuse",
+                ScreeningDecision = "DAIS"
+            };
 
             _mashReferralGateway.InsertDocument(referral1);
             _mashReferralGateway.InsertDocument(referral2);
             _mashReferralGateway.InsertDocument(referral3);
             _mashReferralGateway.InsertDocument(referral4);
             _mashReferralGateway.InsertDocument(referral5);
+            _mashReferralGateway.InsertDocument(referral6);
+            _mashReferralGateway.InsertDocument(referral7);
+            _mashReferralGateway.InsertDocument(referral8);
+            _mashReferralGateway.InsertDocument(referral9);
+            _mashReferralGateway.InsertDocument(referral10);
+            _mashReferralGateway.InsertDocument(referral11);
+            _mashReferralGateway.InsertDocument(referral12);
         }
     }
 }
