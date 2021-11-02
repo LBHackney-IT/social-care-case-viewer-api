9e250a354e931390a5ebc4f55ebd2b1907948517
JohnFarrellDev <johnfarrell@fastmail.com>
2021-11-02 11:25:25 +0000

    custom exception for when mash referal update request does not match to referral current state

--- SocialCareCaseViewerApi/V1/Domain/MashReferral.cs
+++ SocialCareCaseViewerApi/V1/Domain/MashReferral.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Collections.Generic;
 
@@ -11,14 +10,13 @@ namespace SocialCareCaseViewerApi.V1.Domain
 
         public string Referrer { get; set; } = null!;
         public string RequestedSupport { get; set; } = null!;
-
         public Worker? AssignedTo { get; set; }
         public DateTime CreatedAt { get; set; }
         public IEnumerable<string> Clients { get; set; } = null!;
         public string ReferralDocumentURI { get; set; } = null!;
         public string Stage { get; set; } = null!;
         public string? InitialDecision { get; set; }
-        public string? ScreeningDecision { get; set; }
+        public Infrastructure.Screening? Screening { get; set; }
         public string? FinalDecision { get; set; }
         public string? ReferralCategory { get; set; }
     }
