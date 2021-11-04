31cfb81535f648f7d50da5eb967196f54c103ee7
JohnFarrellDev <johnfarrell@fastmail.com>
2021-11-04 19:04:53 +0000

    use case to now patch initial decision answers

--- SocialCareCaseViewerApi/V1/Infrastructure/MashReferral.cs
+++ SocialCareCaseViewerApi/V1/Infrastructure/MashReferral.cs
@@ -23,6 +23,8 @@ namespace SocialCareCaseViewerApi.V1.Infrastructure
         public string ReferralDocumentURI { get; set; } = null!;
         public string Stage { get; set; } = null!;
         public string? InitialDecision { get; set; }
+        public bool? InitialUrgentContactRequired { get; set; }
+        public DateTime? InitialCreatedAt { get; set; }
         public string? ScreeningDecision { get; set; }
         public bool? ScreeningUrgentContactRequired { get; set; }
         public DateTime? ScreeningCreatedAt { get; set; }
