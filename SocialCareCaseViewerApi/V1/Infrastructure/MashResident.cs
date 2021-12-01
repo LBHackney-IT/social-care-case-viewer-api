a6a6036a959d7b31d9f7c6ad8a2ab8e723803082
JohnFarrellDev <johnfarrell@fastmail.com>
2021-12-01 12:36:55 +0000

    formatting

--- SocialCareCaseViewerApi/V1/Infrastructure/MashResident.cs
+++ SocialCareCaseViewerApi/V1/Infrastructure/MashResident.cs
@@ -3,51 +3,53 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 #nullable enable
-namespace SocialCareCaseViewerApi.V1.Infrastructure {
+namespace SocialCareCaseViewerApi.V1.Infrastructure
+{
 
     [Table("ref_mash_residents", Schema = "dbo")]
-    public class MashResident {
+    public class MashResident
+    {
 
         [Column("id")]
         [MaxLength(16)]
         [Key]
-        public long Id {get; set;}
+        public long Id { get; set; }
 
         [Column("first_name")]
-        public string FirstName {get; set;} = null!;
+        public string FirstName { get; set; } = null!;
 
         [Column("last_name")]
-        public string LastName {get; set;} = null!;
+        public string LastName { get; set; } = null!;
 
         [Column("dob")]
-        public DateTime? DateOfBirth {get; set;}
+        public DateTime? DateOfBirth { get; set; }
 
         [Column("gender")]
-        public string? Gender {get; set;}
+        public string? Gender { get; set; }
 
         [Column("ethnicity")]
-        public string? Ethnicity {get; set;}
+        public string? Ethnicity { get; set; }
 
 
         [Column("first_language")]
 
-        public string? FirstLanguage {get; set;}
+        public string? FirstLanguage { get; set; }
 
 
         [Column("school")]
-        public string? School {get; set;}
+        public string? School { get; set; }
 
 
         [Column("address")]
-        public string? Address {get; set;}
+        public string? Address { get; set; }
 
         [Column("postcode")]
-        public string? Postcode {get; set;}
+        public string? Postcode { get; set; }
 
-        [Column ("fk_ref_mash_referrals_id")]
-        public long MashReferralId {get; set;}
+        [Column("fk_ref_mash_referrals_id")]
+        public long MashReferralId { get; set; }
 
-        [ForeignKey ("MashReferralId")]
-        public MashReferral_2 MashReferral {get; set;}
+        [ForeignKey("MashReferralId")]
+        public MashReferral_2 MashReferral { get; set; }
     }
 }
