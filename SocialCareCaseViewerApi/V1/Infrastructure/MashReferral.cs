7aaf622f6f7265ddd002753c06bfd84f8653aee4
JohnFarrellDev <johnfarrell@fastmail.com>
2021-12-10 15:12:15 +0000

    update MashReferral infrastructure to join with Worker table

--- SocialCareCaseViewerApi/V1/Infrastructure/MashReferral.cs
+++ SocialCareCaseViewerApi/V1/Infrastructure/MashReferral.cs
@@ -14,39 +14,59 @@ namespace SocialCareCaseViewerApi.V1.Infrastructure
         [MaxLength(16)]
         [Key]
         public long Id { get; set; }
+
+
+        [Column("worker_id")]
+        public long? WorkerId { get; set; }
+
+        [ForeignKey("WorkerId")]
+        public Worker Worker { get; set; }
+
         [Column("referrer")]
         public string Referrer { get; set; } = null!;
         [Column("requested_support")]
         public string RequestedSupport { get; set; } = null!;
+
         [Column("created_at")]
         public DateTime ReferralCreatedAt { get; set; }
 
+
         [Column("referral_doc_url")]
         public string ReferralDocumentURI { get; set; } = null!;
+
         [Column("stage")]
         public string Stage { get; set; } = null!;
+
         [Column("referral_category")]
         public string? ReferralCategory { get; set; }
+
         [Column("contact_decision_created_at")]
         public DateTime? ContactDecisionCreatedAt { get; set; }
+
         [Column("contact_decision_urgent_contact")]
         public bool? ContactDecisionUrgentContactRequired { get; set; }
 
         [Column("initial_decision")]
         public string? InitialDecision { get; set; }
+
         [Column("initial_decision_referral_category")]
         public string? InitialDecisionReferralCategory { get; set; }
+
         [Column("initial_decision_urgent_contact")]
         public bool? InitialDecisionUrgentContactRequired { get; set; }
+
         [Column("initial_decision_created_at")]
         public DateTime? InitialDecisionCreatedAt { get; set; }
 
         [Column("screening_decision")]
         public string? ScreeningDecision { get; set; }
+
         [Column("screening_decision_urgent_contact")]
         public bool? ScreeningUrgentContactRequired { get; set; }
+
         [Column("screening_decision_created_at")]
         public DateTime? ScreeningCreatedAt { get; set; }
+
         [Column("final_decision")]
         public string? FinalDecision { get; set; }
         [Column("final_decision_referral_category")]
