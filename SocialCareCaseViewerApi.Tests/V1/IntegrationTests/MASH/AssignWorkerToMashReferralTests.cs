147644ad8b6288ebfaca4d49929ba3019e407fbf
Juris Gribuska <juris.gribuska@hackney.gov.uk>
2021-12-15 16:38:04 +0000

    unfinished integration test for assigning worker to referral

--- SocialCareCaseViewerApi.Tests/V1/IntegrationTests/MASH/AssignWorkerToMashReferralTests.cs
+++ SocialCareCaseViewerApi.Tests/V1/IntegrationTests/MASH/AssignWorkerToMashReferralTests.cs
@@ -14,12 +14,37 @@ using JsonSerializer = System.Text.Json.JsonSerializer;
 namespace SocialCareCaseViewerApi.Tests.V1.IntegrationTests.MASH
 {
     [TestFixture]
-    public class CreateNewMashReferralTests : IntegrationTestSetup<Startup>
+    public class AssignWorkerToMashReferralTests : IntegrationTestSetup<Startup>
     {
+        private SocialCareCaseViewerApi.V1.Infrastructure.Worker _existingDbWorker;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Clear test database of any rows in the database
+            DatabaseContext.Teams.RemoveRange(DatabaseContext.Teams);
+            DatabaseContext.Workers.RemoveRange(DatabaseContext.Workers);
+            DatabaseContext.MashReferrals.RemoveRange(DatabaseContext.MashReferrals);
+
+            // Create existing workers with teams
+            var (existingDbWorker, _) = IntegrationTestHelpers.SetupExistingWorker(DatabaseContext);
+            _existingDbWorker = existingDbWorker;
+        }
+
         [Test]
         public async Task SuccessfulPatchAssignsWorkerToReferral()
         {
+            var request = TestHelpers.CreateUpdateMashReferral();
+            request.WorkerId = _existingDbWorker.Id;
+            request.WorkerEmail = null;
+            request.UpdateType = "ASSIGN-WORKER";
+            var postUri = new Uri($"/api/v1/mash-referral/{refferal.Id}", UriKind.Relative);
+            var serializedRequest = JsonSerializer.Serialize(request);
+            var requestContent = new StringContent(serializedRequest, Encoding.UTF8, "application/json");
 
+            var updatedMashReferralResponse = await Client.PatchAsync(postUri, requestContent).ConfigureAwait(true);
+
+            updatedMashReferralResponse.StatusCode.Should().Be(201);
         }
     }
 }
