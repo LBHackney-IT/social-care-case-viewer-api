2a86127fb6f63138998ef63e71cc6a367d0481fe
Juris Gribuska <juris.gribuska@hackney.gov.uk>
2022-01-04 13:09:01 +0000

    better formatting

--- SocialCareCaseViewerApi/V1/Boundary/Requests/UpdateMashReferral.cs
+++ SocialCareCaseViewerApi/V1/Boundary/Requests/UpdateMashReferral.cs
@@ -1,4 +1,3 @@
-
 using FluentValidation;
 
 #nullable enable
@@ -20,43 +19,51 @@ namespace SocialCareCaseViewerApi.V1.Boundary.Requests
         {
             RuleFor(x => x.Decision)
                 .NotNull()
-                .When(x => x.UpdateType == "INITIAL-DECISION" || x.UpdateType == "SCREENING-DECISION" || x.UpdateType == "FINAL-DECISION")
-                    .WithMessage("Must provide a decision");
+                .When(x => x.UpdateType == "INITIAL-DECISION" || x.UpdateType == "SCREENING-DECISION" ||
+                           x.UpdateType == "FINAL-DECISION")
+                .WithMessage("Must provide a decision");
             RuleFor(x => x.Decision)
                 .MinimumLength(1)
-                    .When(x => x.UpdateType == "INITIAL-DECISION" || x.UpdateType == "SCREENING-DECISION" || x.UpdateType == "FINAL-DECISION")
+                .When(x => x.UpdateType == "INITIAL-DECISION" || x.UpdateType == "SCREENING-DECISION" ||
+                           x.UpdateType == "FINAL-DECISION")
                 .WithMessage("Must provide a decision");
             RuleFor(x => x.ReferralCategory)
                 .NotNull()
-                    .When(x => x.UpdateType == "INITIAL-DECISION" || x.UpdateType == "FINAL-DECISION")
-                    .WithMessage("Must provide a referral category");
+                .When(x => x.UpdateType == "INITIAL-DECISION" || x.UpdateType == "FINAL-DECISION")
+                .WithMessage("Must provide a referral category");
             RuleFor(x => x.ReferralCategory)
                 .MinimumLength(1)
-                    .When(x => x.UpdateType == "INITIAL-DECISION" || x.UpdateType == "FINAL-DECISION")
-                    .WithMessage("Must provide a referral category");
+                .When(x => x.UpdateType == "INITIAL-DECISION" || x.UpdateType == "FINAL-DECISION")
+                .WithMessage("Must provide a referral category");
             RuleFor(x => x.RequiresUrgentContact)
                 .NotNull()
                 .When(x => x.UpdateType == "CONTACT-DECISION" || x.UpdateType == "INITIAL-DECISION" ||
                            x.UpdateType == "SCREENING-DECISION" || x.UpdateType == "FINAL-DECISION")
                 .WithMessage("Must provide if urgent contact is required");
-            RuleFor(x => x.WorkerId )
+            RuleFor(x => x.WorkerId)
                 .NotNull()
                 .GreaterThan(0)
-                .When(x => x.WorkerEmail == null && x.WorkerId != null && (x.UpdateType == "ASSIGN-WORKER" || x.UpdateType == "CONTACT-DECISION"))
+                .When(x => x.WorkerEmail == null && x.WorkerId != null &&
+                           (x.UpdateType == "ASSIGN-WORKER" || x.UpdateType == "CONTACT-DECISION"))
                 .WithMessage("Must provide a valid worker id or email");
             RuleFor(x => x.WorkerEmail)
                 .NotNull()
                 .EmailAddress()
-                .When(x => x.WorkerId == null && x.WorkerEmail != null && (x.UpdateType == "ASSIGN-WORKER" || x.UpdateType == "CONTACT-DECISION"))
+                .When(x => x.WorkerId == null && x.WorkerEmail != null &&
+                           (x.UpdateType == "ASSIGN-WORKER" || x.UpdateType == "CONTACT-DECISION"))
                 .WithMessage("Must provide a valid worker id or email");
-            When(x => x.WorkerEmail == null && x.WorkerId == null && (x.UpdateType == "ASSIGN-WORKER" || x.UpdateType == "CONTACT-DECISION"), () =>
-            {
-                RuleFor(x => x.WorkerId).NotNull().WithMessage("Must provide a valid worker id or email");
-            });
-            When(x => x.WorkerEmail != null && x.WorkerId != null && (x.UpdateType == "ASSIGN-WORKER" || x.UpdateType == "CONTACT-DECISION"), () =>
-            {
-                RuleFor(x => x.WorkerId).Null().WithMessage("Do not provide both worker id and worker email address");
-            });
+            When(
+                x => x.WorkerEmail == null && x.WorkerId == null &&
+                     (x.UpdateType == "ASSIGN-WORKER" || x.UpdateType == "CONTACT-DECISION"),
+                () => { RuleFor(x => x.WorkerId).NotNull().WithMessage("Must provide a valid worker id or email"); });
+            When(
+                x => x.WorkerEmail != null && x.WorkerId != null &&
+                     (x.UpdateType == "ASSIGN-WORKER" || x.UpdateType == "CONTACT-DECISION"),
+                () =>
+                {
+                    RuleFor(x => x.WorkerId).Null()
+                        .WithMessage("Do not provide both worker id and worker email address");
+                });
         }
     }
 }
