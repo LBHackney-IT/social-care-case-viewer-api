d72a5f7c5bdc011172806d03a762819971ba9cfe
Renny Fadoju <32823756+Renny4Real@users.noreply.github.com>
2022-01-13 16:47:23 +0000

    Remove unneeded validator for now
    Since the SocialCareId is not nullable, validating the request on whether the Social Care Id parameter is null, does not seem to be needed.

--- SocialCareCaseViewerApi/V1/Boundary/Requests/UpdateMashResidentRequest.cs
+++ SocialCareCaseViewerApi/V1/Boundary/Requests/UpdateMashResidentRequest.cs
@@ -1,17 +1,8 @@
-using FluentValidation;
-
+#nullable enable
 namespace SocialCareCaseViewerApi.V1.Boundary.Requests
 {
     public class UpdateMashResidentRequest
     {
         public long SocialCareId { get; set; }
-    }
-
-    public class UpdateMashResidentRequestValidator : AbstractValidator<UpdateMashResidentRequest>
-    {
-        public UpdateMashResidentRequestValidator()
-        {
-            RuleFor(x => x.SocialCareId).NotNull().WithMessage("Must provide a social care ID");
-        }
     }
 }
