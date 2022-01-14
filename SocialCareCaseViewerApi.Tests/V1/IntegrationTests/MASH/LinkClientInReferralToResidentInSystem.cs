a1e733b7a7c6cb0d646cd59cd198c557dc7a20c7
Juris Gribuska <juris.gribuska@hackney.gov.uk>
2022-01-14 13:49:35 +0000

    removed redundant tests

--- SocialCareCaseViewerApi.Tests/V1/IntegrationTests/MASH/LinkClientInReferralToResidentInSystem.cs
+++ SocialCareCaseViewerApi.Tests/V1/IntegrationTests/MASH/LinkClientInReferralToResidentInSystem.cs
@@ -51,55 +51,5 @@ namespace SocialCareCaseViewerApi.Tests.V1.IntegrationTests.MASH
 
             patchMashResidentResponse.SocialCareId.Should().Be(_existingDbPerson.Id);
         }
-
-        // [Test]
-        // public async Task WhenTheRequestIsInvalidReturnsBadRequestStatusCode()
-        // {
-        //     const string request = "invalid request";
-        //     var patchUri = new Uri($"/api/v1/mash-resident/{_unlinkedResident.Id}", UriKind.Relative);
-        //     var serializedRequest = JsonSerializer.Serialize(request);
-        //     var requestContent = new StringContent(serializedRequest, Encoding.UTF8, "application/json");
-        //
-        //     var response = await Client.PatchAsync(patchUri, requestContent).ConfigureAwait(true);
-        //
-        //     response.StatusCode.Should().Be(400);
-        // }
-        //
-        // [Test]
-        // public async Task WhenTheRequestContainsAnIncorrectPersonIdReturnsRelevantErrorMessage()
-        // {
-        //     var incorrectPersonId = _existingDbPerson.Id + 20;
-        //     var request = new UpdateMashResidentRequest { SocialCareId = incorrectPersonId };
-        //     var patchUri = new Uri($"/api/v1/mash-resident/{_unlinkedResident.Id}", UriKind.Relative);
-        //     var serializedRequest = JsonSerializer.Serialize(request);
-        //     var requestContent = new StringContent(serializedRequest, Encoding.UTF8, "application/json");
-        //
-        //     var response = await Client.PatchAsync(patchUri, requestContent).ConfigureAwait(true);
-        //
-        //     response.StatusCode.Should().Be(400);
-        //     var content = await response.Content.ReadAsStringAsync().ConfigureAwait(true);
-        //     var patchResponse = JsonConvert.DeserializeObject<string>(content);
-        //
-        //     patchResponse.Should().Be($"Person with id {incorrectPersonId} not found");
-        // }
-        //
-        //
-        // [Test]
-        // public async Task WhenTheRequestContainsAnIncorrectMashResidentIdReturnsRelevantErrorMessage()
-        // {
-        //     var incorrectMashResidentId = _unlinkedResident.Id + 20;
-        //     var request = new UpdateMashResidentRequest { SocialCareId = _existingDbPerson.Id };
-        //     var patchUri = new Uri($"/api/v1/mash-resident/{incorrectMashResidentId}", UriKind.Relative);
-        //     var serializedRequest = JsonSerializer.Serialize(request);
-        //     var requestContent = new StringContent(serializedRequest, Encoding.UTF8, "application/json");
-        //
-        //     var response = await Client.PatchAsync(patchUri, requestContent).ConfigureAwait(true);
-        //
-        //     response.StatusCode.Should().Be(400);
-        //     var content = await response.Content.ReadAsStringAsync().ConfigureAwait(true);
-        //     var patchResponse = JsonConvert.DeserializeObject<string>(content);
-        //
-        //     patchResponse.Should().Be($"MASH resident with id {incorrectMashResidentId} not found");
-        // }
     }
 }
