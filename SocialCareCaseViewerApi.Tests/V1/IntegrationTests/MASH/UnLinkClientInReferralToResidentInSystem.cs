048beb90fc88f9f9a467800700d425a9ce8ab84e
Juris Gribuska <juris.gribuska@hackney.gov.uk>
2022-01-14 14:28:54 +0000

    correctly failing integration test

--- SocialCareCaseViewerApi.Tests/V1/IntegrationTests/MASH/UnLinkClientInReferralToResidentInSystem.cs
+++ SocialCareCaseViewerApi.Tests/V1/IntegrationTests/MASH/UnLinkClientInReferralToResidentInSystem.cs
@@ -14,10 +14,10 @@ using MashReferral = SocialCareCaseViewerApi.V1.Infrastructure.MashReferral;
 namespace SocialCareCaseViewerApi.Tests.V1.IntegrationTests.MASH
 {
     [TestFixture]
-    public class LinkClientInReferralToResidentInSystem : IntegrationTestSetup<Startup>
+    public class UnLinkClientInReferralToResidentInSystem : IntegrationTestSetup<Startup>
     {
         private MashReferral _mashReferral;
-        private MashResident _unlinkedResident;
+        private MashResident _linkedResident;
         private Person _existingDbPerson;
 
         [SetUp]
@@ -26,79 +26,25 @@ namespace SocialCareCaseViewerApi.Tests.V1.IntegrationTests.MASH
             _existingDbPerson = IntegrationTestHelpers.CreateExistingPerson(DatabaseContext);
             _mashReferral = IntegrationTestHelpers.SaveMashReferralToDatabase(DatabaseContext, "CONTACT");
 
-            _unlinkedResident = IntegrationTestHelpers.CreateUnLinkedMashResident(DatabaseContext, _mashReferral, _existingDbPerson);
+            _linkedResident =
+                IntegrationTestHelpers.CreateMashResident(DatabaseContext, _mashReferral, _existingDbPerson);
         }
 
         [Test]
-        public async Task SuccessfulLinkingMatchesMashResidentToSavedPersonInDb()
+        public async Task SuccessfulUnLinkingRemovesLinkFromMashResidentToSavedPersonInDb()
         {
-            _unlinkedResident.SocialCareId.Should().BeNull();
+            _linkedResident.SocialCareId.Should().NotBeNull();
 
-            var request = new UpdateMashResidentRequest { SocialCareId = _existingDbPerson.Id };
-            var patchUri = new Uri($"/api/v1/mash-resident/{_unlinkedResident.Id}", UriKind.Relative);
+            var request = new UpdateMashResidentRequest { UpdateType = "UNLINK-PERSON"};
+            var patchUri = new Uri($"/api/v1/mash-resident/{_linkedResident.Id}", UriKind.Relative);
             var serializedRequest = JsonSerializer.Serialize(request);
             var requestContent = new StringContent(serializedRequest, Encoding.UTF8, "application/json");
 
-            var linkMashResidentResponse = await Client.PatchAsync(patchUri, requestContent).ConfigureAwait(true);
+            var unlinkMashResidentResponse = await Client.PatchAsync(patchUri, requestContent).ConfigureAwait(true);
 
-            linkMashResidentResponse.StatusCode.Should().Be(200);
+            unlinkMashResidentResponse.StatusCode.Should().Be(200);
 
-            _unlinkedResident.SocialCareId.Should().Be(_existingDbPerson.Id);
-
-            var content = await linkMashResidentResponse.Content.ReadAsStringAsync().ConfigureAwait(true);
-            var patchMashResidentResponse = JsonConvert.DeserializeObject<MashResidentResponse>(content);
-
-            patchMashResidentResponse.SocialCareId.Should().Be(_existingDbPerson.Id);
-        }
-
-        [Test]
-        public async Task WhenTheRequestIsInvalidReturnsBadRequestStatusCode()
-        {
-            const string request = "invalid request";
-            var patchUri = new Uri($"/api/v1/mash-resident/{_unlinkedResident.Id}", UriKind.Relative);
-            var serializedRequest = JsonSerializer.Serialize(request);
-            var requestContent = new StringContent(serializedRequest, Encoding.UTF8, "application/json");
-
-            var response = await Client.PatchAsync(patchUri, requestContent).ConfigureAwait(true);
-
-            response.StatusCode.Should().Be(400);
-        }
-
-        [Test]
-        public async Task WhenTheRequestContainsAnIncorrectPersonIdReturnsRelevantErrorMessage()
-        {
-            var incorrectPersonId = _existingDbPerson.Id + 20;
-            var request = new UpdateMashResidentRequest { SocialCareId = incorrectPersonId };
-            var patchUri = new Uri($"/api/v1/mash-resident/{_unlinkedResident.Id}", UriKind.Relative);
-            var serializedRequest = JsonSerializer.Serialize(request);
-            var requestContent = new StringContent(serializedRequest, Encoding.UTF8, "application/json");
-
-            var response = await Client.PatchAsync(patchUri, requestContent).ConfigureAwait(true);
-
-            response.StatusCode.Should().Be(400);
-            var content = await response.Content.ReadAsStringAsync().ConfigureAwait(true);
-            var patchResponse = JsonConvert.DeserializeObject<string>(content);
-
-            patchResponse.Should().Be($"Person with id {incorrectPersonId} not found");
-        }
-
-
-        [Test]
-        public async Task WhenTheRequestContainsAnIncorrectMashResidentIdReturnsRelevantErrorMessage()
-        {
-            var incorrectMashResidentId = _unlinkedResident.Id + 20;
-            var request = new UpdateMashResidentRequest { SocialCareId = _existingDbPerson.Id };
-            var patchUri = new Uri($"/api/v1/mash-resident/{incorrectMashResidentId}", UriKind.Relative);
-            var serializedRequest = JsonSerializer.Serialize(request);
-            var requestContent = new StringContent(serializedRequest, Encoding.UTF8, "application/json");
-
-            var response = await Client.PatchAsync(patchUri, requestContent).ConfigureAwait(true);
-
-            response.StatusCode.Should().Be(400);
-            var content = await response.Content.ReadAsStringAsync().ConfigureAwait(true);
-            var patchResponse = JsonConvert.DeserializeObject<string>(content);
-
-            patchResponse.Should().Be($"MASH resident with id {incorrectMashResidentId} not found");
+            _linkedResident.SocialCareId.Should().BeNull();
         }
     }
 }
