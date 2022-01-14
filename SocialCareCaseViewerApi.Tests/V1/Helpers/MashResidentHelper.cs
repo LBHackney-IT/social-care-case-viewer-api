624632a0341218210a3b8925d4cecbcf5917490b
Juris Gribuska <juris.gribuska@hackney.gov.uk>
2022-01-14 15:41:35 +0000

    saving resident to db helper now sets the social care id

--- SocialCareCaseViewerApi.Tests/V1/Helpers/MashResidentHelper.cs
+++ SocialCareCaseViewerApi.Tests/V1/Helpers/MashResidentHelper.cs
@@ -4,9 +4,10 @@ namespace SocialCareCaseViewerApi.Tests.V1.Helpers
 {
     public static class MashResidentHelper
     {
-        public static MashResident SaveMashResidentToDatabase(DatabaseContext databaseContext, long? mashReferralId = null)
+        public static MashResident SaveMashResidentToDatabase(DatabaseContext databaseContext, long? mashReferralId = null, long? socialCareId = null)
         {
             var resident = TestHelpers.CreateMashResident(mashReferralId);
+            resident.SocialCareId = socialCareId;
             databaseContext.MashResidents.Add(resident);
             databaseContext.SaveChanges();
             return resident;
