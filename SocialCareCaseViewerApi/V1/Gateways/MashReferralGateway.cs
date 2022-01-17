ccefeef0c472acf80611e233cc4a27bf7d013c69
Juris Gribuska <juris.gribuska@hackney.gov.uk>
2022-01-17 15:04:48 +0000

    simplified logic

--- SocialCareCaseViewerApi/V1/Gateways/MashReferralGateway.cs
+++ SocialCareCaseViewerApi/V1/Gateways/MashReferralGateway.cs
@@ -81,8 +81,7 @@ namespace SocialCareCaseViewerApi.V1.Gateways
         public IEnumerable<MashReferral> GetReferralsUsingQuery(QueryMashReferrals request)
         {
             var results = _databaseContext.MashReferrals.AsQueryable();
-
-            if (request.WorkerEmail != null && request.Id == null)
+            if (request.WorkerEmail != null)
             {
                 results = results.Where(x => x.Worker.Email == request.WorkerEmail);
             }
