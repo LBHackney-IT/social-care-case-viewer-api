7b6d7a8fcb3fd367c753ed037781dd6cffac7fc1
Juris Gribuska <juris.gribuska@hackney.gov.uk>
2022-01-21 11:04:11 +0000

    removed mash resident request

--- SocialCareCaseViewerApi/V1/Boundary/Requests/UpdateMashResidentRequest.cs
+++
@@ -1,9 +0,0 @@
-#nullable enable
-namespace SocialCareCaseViewerApi.V1.Boundary.Requests
-{
-    public class UpdateMashResidentRequest
-    {
-        public long? SocialCareId { get; set; }
-        public string? UpdateType { get; set; }
-    }
-}
