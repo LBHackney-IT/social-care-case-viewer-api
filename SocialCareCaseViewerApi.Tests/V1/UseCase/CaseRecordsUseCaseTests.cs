874c537cc9c8cc90e88f1670bffa8df2852cc707
Juris Gribuska <juris.gribuska@hackney.gov.uk>
2022-02-09 08:54:59 +0000

    testing the total count is subbmited in the request

--- SocialCareCaseViewerApi.Tests/V1/UseCase/CaseRecordsUseCaseTests.cs
+++ SocialCareCaseViewerApi.Tests/V1/UseCase/CaseRecordsUseCaseTests.cs
@@ -35,6 +35,31 @@ namespace SocialCareCaseViewerApi.Tests.V1.UseCase
             _caseRecordsUseCase = new CaseRecordsUseCase(_mockProcessDataGateway.Object, _mockDatabaseGateWay.Object, _mockMongoGateway.Object);
         }
 
+                [Test]
+        public void GetResidentCasesReturnsResidentsSubmittedCasesWithTotalCount()
+        {
+            var request = TestHelpers.CreateListCasesRequest(1L);
+
+            var expectedResponse = new List<CaseSubmission>
+            {
+                TestHelpers.CreateCaseSubmission(SubmissionState.Submitted, residentId: int.Parse(request.MosaicId ?? "")),
+                TestHelpers.CreateCaseSubmission(SubmissionState.Submitted, residentId: int.Parse(request.MosaicId ?? "")),
+                TestHelpers.CreateCaseSubmission(SubmissionState.Submitted, residentId: int.Parse(request.MosaicId ?? ""))
+            };
+
+            _mockDatabaseGateWay.Setup(x => x.GetNCReferenceByPersonId(request.MosaicId)).Returns(request.MosaicId ?? "");
+            _mockDatabaseGateWay.Setup(x => x.GetPersonIdByNCReference(request.MosaicId)).Returns(request.MosaicId ?? "");
+            _mockProcessDataGateway.Setup(x => x.GetProcessData(request, request.MosaicId)).Returns(() => new Tuple<IEnumerable<CareCaseData>, int>(new List<CareCaseData>(), 0));
+            _mockMongoGateway
+                .Setup(x => x.LoadRecordsByFilter(MongoConnectionStrings.Map[Collection.ResidentCaseSubmissions],
+                    It.IsAny<FilterDefinition<CaseSubmission>>(), It.IsAny<Pagination>()))
+                .Returns((expectedResponse, 2));
+
+            var response = _caseRecordsUseCase.GetResidentCases(request);
+
+            response.TotalCount.Should().Be(3);
+        }
+
         [Test]
         public void GetResidentCasesReturnsResidentsSubmittedCasesExcludingAuditTrailEventsWhenExcludeAuditTrailEventsIsSetToTrue()
         {
