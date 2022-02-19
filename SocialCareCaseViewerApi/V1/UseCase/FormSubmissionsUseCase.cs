a9a621e79d30372c57e0bb357179022f84ffefaf
Juris Gribuska <juris.gribuska@hackney.gov.uk>
2022-02-19 12:07:42 +0000

    setting pinned at correctly

--- SocialCareCaseViewerApi/V1/UseCase/FormSubmissionsUseCase.cs
+++ SocialCareCaseViewerApi/V1/UseCase/FormSubmissionsUseCase.cs
@@ -217,22 +217,14 @@ namespace SocialCareCaseViewerApi.V1.UseCase
                 throw new GetSubmissionException($"Submission with ID {submissionId} not found");
             }
 
-            if (updatedSubmission.PinnedAt == null)
-            {
-                throw new GetSubmissionException($"Submission with ID {submissionId} not found");
-            }
-
             UpdateSubmissionState(updatedSubmission, request, worker);
             UpdateResidents(updatedSubmission, request);
 
-            if (updatedSubmission.PinnedAt.HasValue)
+            if (request.PinnedAt != null)
             {
-                updatedSubmission.PinnedAt = DateTime.Parse(request.PinnedAt);
+                updatedSubmission.PinnedAt = String.IsNullOrEmpty(request.PinnedAt) ? (DateTime?) null : DateTime.Parse(request.PinnedAt);
             }
-            else
-            {
-                updatedSubmission.PinnedAt = null;
-            }
+
 
             updatedSubmission.EditHistory.Add(new EditHistory<Worker> { Worker = worker, EditTime = DateTime.Now });
 
