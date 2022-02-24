b695574781c82ac35b1123efb85d94010c72e858
Juris Gribuska <juris.gribuska@hackney.gov.uk>
2022-02-24 11:02:04 +0000

    added residentworker infra

---
+++ SocialCareCaseViewerApi/V1/Infrastructure/ResidentWorker.cs
@@ -0,0 +1,29 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SocialCareCaseViewerApi.V1.Infrastructure
+{
+    [Table("sccv_residentworker", Schema = "dbo")]
+    public class ResidentWorker
+    {
+        [Key]
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [ForeignKey("PersonId")]
+        public Person Person { get; set; }
+
+        [Column("person_id")]
+        [Required]
+        public long PersonId { get; set; }
+
+        [Column("worker_id")]
+        public int WorkerId { get; set; }
+        public Worker Worker { get; set; }
+
+        [Column("updated_at")]
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
