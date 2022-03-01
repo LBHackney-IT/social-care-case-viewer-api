e1a1e280bb1ccd784d9c7607a67c9d251f90dff1
Juris Gribuska <juris.gribuska@hackney.gov.uk>
2022-03-01 10:11:24 +0000

    testing _residentUseCase.AllocateResidentToTheTeam code

---
+++ SocialCareCaseViewerApi.Tests/V1/UseCase/Residents/AllocateResidentTests.cs
@@ -0,0 +1,42 @@
+using Bogus;
+using Moq;
+using NUnit.Framework;
+using SocialCareCaseViewerApi.V1.Boundary.Requests;
+using SocialCareCaseViewerApi.V1.Gateways.Interfaces;
+using SocialCareCaseViewerApi.V1.UseCase;
+
+#nullable enable
+namespace SocialCareCaseViewerApi.Tests.V1.UseCase.Residents
+{
+    [TestFixture]
+    public class AllocateResidentTests
+    {
+        private Mock<IDatabaseGateway> _mockDataBaseGateway = null!;
+        private ResidentUseCase _residentUseCase = null!;
+        private readonly Faker _faker = new Faker();
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockDataBaseGateway = new Mock<IDatabaseGateway>();
+            _residentUseCase = new ResidentUseCase(_mockDataBaseGateway.Object);
+        }
+
+        private AllocateResidentToTheTeamRequest GetValidAllocateResidentToTheTeamRequest()
+        {
+            return new Faker<AllocateResidentToTheTeamRequest>()
+                .RuleFor(x => x.PersonId, _faker.Random.Long())
+                .RuleFor(x => x.AllocatedTeamId, _faker.Random.Int());
+        }
+
+        [Test]
+        public void ExecutePatchCallsDatabaseGateway()
+        {
+            var request = GetValidAllocateResidentToTheTeamRequest();
+
+            _residentUseCase.AllocateResidentToTheTeam(request);
+
+            _mockDataBaseGateway.Verify(x => x.AllocateResidentToTheTeam(request));
+        }
+    }
+}
