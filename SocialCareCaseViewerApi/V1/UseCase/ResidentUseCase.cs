f335116ef189ed6a3afff38787367c1fadd174a1
Juris Gribuska <juris.gribuska@hackney.gov.uk>
2022-03-01 14:13:38 +0000

    redundant import

--- SocialCareCaseViewerApi/V1/UseCase/ResidentUseCase.cs
+++ SocialCareCaseViewerApi/V1/UseCase/ResidentUseCase.cs
@@ -5,7 +5,6 @@ using SocialCareCaseViewerApi.V1.Boundary.Requests;
 using SocialCareCaseViewerApi.V1.Boundary.Response;
 using SocialCareCaseViewerApi.V1.Factories;
 using SocialCareCaseViewerApi.V1.Gateways.Interfaces;
-using SocialCareCaseViewerApi.V1.Infrastructure;
 using SocialCareCaseViewerApi.V1.UseCase.Interfaces;
 
 #nullable enable
