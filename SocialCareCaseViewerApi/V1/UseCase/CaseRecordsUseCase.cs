6b62f939e017faebc6114cec818b5be00e986189
Juris Gribuska <juris.gribuska@hackney.gov.uk>
2022-03-02 14:49:09 +0000

    found the bug

--- SocialCareCaseViewerApi/V1/UseCase/CaseRecordsUseCase.cs
+++ SocialCareCaseViewerApi/V1/UseCase/CaseRecordsUseCase.cs
@@ -123,6 +123,8 @@ namespace SocialCareCaseViewerApi.V1.UseCase
                     .Skip(request.Cursor)
                     .Take(request.Limit)
                     .ToList();
+
+                combinedCases.Skip(request.Cursor).Take(request.Limit).ToList();
             }
 
             int? nextCursor = request.Cursor + request.Limit;
