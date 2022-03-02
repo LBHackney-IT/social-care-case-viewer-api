b11d27a848645014769e8b4b570a3feb5fb817ab
Juris Gribuska <juris.gribuska@hackney.gov.uk>
2022-03-02 11:00:18 +0000

    inlcuding resident cases in the tean controller tests

--- SocialCareCaseViewerApi.Tests/V1/Controllers/TeamsControllerTests.cs
+++ SocialCareCaseViewerApi.Tests/V1/Controllers/TeamsControllerTests.cs
@@ -18,12 +18,14 @@ namespace SocialCareCaseViewerApi.Tests.V1.Controllers
     {
         private TeamController _teamController;
         private Mock<ITeamsUseCase> _teamsUseCase;
+        private Mock<IResidentUseCase> _residentUseCase;
 
         [SetUp]
         public void Setup()
         {
             _teamsUseCase = new Mock<ITeamsUseCase>();
-            _teamController = new TeamController(_teamsUseCase.Object);
+            _residentUseCase = new Mock<IResidentUseCase>();
+            _teamController = new TeamController(_teamsUseCase.Object, _residentUseCase.Object);
         }
 
         [Test]
