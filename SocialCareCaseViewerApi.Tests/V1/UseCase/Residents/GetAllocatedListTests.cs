727994c38be7536c08d27ad919912b706c29bbcb
Juris Gribuska <juris.gribuska@hackney.gov.uk>
2022-03-09 14:07:21 +0000

    merged tests into one

--- SocialCareCaseViewerApi.Tests/V1/UseCase/Residents/GetAllocatedListTests.cs
+++ SocialCareCaseViewerApi.Tests/V1/UseCase/Residents/GetAllocatedListTests.cs
@@ -7,6 +7,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
+using SocialCareCaseViewerApi.Tests.V1.Helpers;
+using SocialCareCaseViewerApi.V1.Factories;
 using SocialCareCaseViewerApi.V1.Gateways.Interfaces;
 
 #nullable enable
@@ -37,9 +39,30 @@ namespace SocialCareCaseViewerApi.Tests.V1.UseCase.Residents
                 .BeEquivalentTo(new ResidentInformationList() { Residents = new List<ResidentInformation>(), NextCursor = "", TotalCount = 0 });
         }
 
+        [Test]
+        public void ReturnsListOfResidentsAllocatedToTheTeam()
+        {
+            var person = TestHelpers.CreatePerson();
+            var residentTeam = new ResidentTeam()
+            {
+                TeamId = 1,
+                RagRating = "Green"
+            };
+
+            person.ResidentTeams = new List<ResidentTeam>() { residentTeam };
+            var residents = new List<ResidentInformation>();
+            residents.Add(person.ToResidentInformationResponse());
+
+            _mockDatabaseGateway.Setup(x => x.GetPersonsByTeamId(1, "unallocated")).Returns(new List<Person>() { person });
+            _residentUseCase
+                .GetAllocatedList(1, "unallocated", cursor: 0, limit: 20)
+                .Should()
+                .BeEquivalentTo(new ResidentInformationList() { Residents = residents, NextCursor = "", TotalCount = 1 });
+        }
+
 
         [Test]
-        public void ReturnsListWhenTeamHasResidentsAssigned()
+        public void ReturnsOrderedListByRagRatingThenByTimeWhenTeamHasResidentsAssigned()
         {
             var residentsList = new List<Person>();
             var ragList = new List<string> { "purple", "red", "amber", "green", "purple", "red", "amber", "green", };
