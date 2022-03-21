083ff24539601d24cbd2e28d4d957c53b695b337
Marta Pederiva <86296201+martapederiva@users.noreply.github.com>
2022-03-21 11:33:46 +0000

    expanded SelectAllocationByTeamId in DBGateway tests

--- SocialCareCaseViewerApi.Tests/V1/Gateways/DatabaseGateway.Tests.cs
+++ SocialCareCaseViewerApi.Tests/V1/Gateways/DatabaseGateway.Tests.cs
@@ -289,19 +289,33 @@ namespace SocialCareCaseViewerApi.Tests.V1.Gateways
         {
             // Create worker and teams
             var worker = TestHelpers.CreateWorker(hasAllocations: false, hasWorkerTeams: false, id: 123);
+            var anotherWorker = TestHelpers.CreateWorker(hasAllocations: false, hasWorkerTeams: false, id: 124);
+
             var workerTeam = TestHelpers.CreateWorkerTeam(worker.Id);
+            var anotherWorkerTeam = TestHelpers.CreateWorkerTeam(anotherWorker.Id);
+
             worker.WorkerTeams = new List<WorkerTeam> { workerTeam };
+            anotherWorker.WorkerTeams = new List<WorkerTeam> { anotherWorkerTeam };
+
 
             DatabaseContext.Workers.Add(worker);
             DatabaseContext.WorkerTeams.Add(workerTeam);
+            DatabaseContext.Workers.Add(anotherWorker);
+            DatabaseContext.WorkerTeams.Add(anotherWorkerTeam);
             DatabaseContext.SaveChanges();
 
             var (createAllocationRequest, _, allocator, resident, _) = TestHelpers.CreateAllocationRequest(workerId: worker.Id, teamId: workerTeam.TeamId);
+            var (createAnotherAllocationRequest, _, anotherAllocator, anotherResident, _) = TestHelpers.CreateAllocationRequest(workerId: anotherWorker.Id, teamId: anotherWorkerTeam.TeamId);
+
             DatabaseContext.Workers.Add(allocator);
             DatabaseContext.Persons.Add(resident);
+            DatabaseContext.Workers.Add(anotherAllocator);
+            DatabaseContext.Persons.Add(anotherResident);
             DatabaseContext.SaveChanges();
 
             _classUnderTest.CreateAllocation(createAllocationRequest);
+            _classUnderTest.CreateAllocation(createAnotherAllocationRequest);
+
 
             var allocations = _classUnderTest.SelectAllocations(0, 0, null, workerTeam.TeamId);
 
