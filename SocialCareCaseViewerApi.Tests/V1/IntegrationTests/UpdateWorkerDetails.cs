df62dbf66f02c9d8d3815d18c672801c514e9b6d
Juris Gribuska <juris.gribuska@hackney.gov.uk>
2022-03-22 06:51:47 +0000

    refactored intergation test to insert second allocation directly to the DB

--- SocialCareCaseViewerApi.Tests/V1/IntegrationTests/UpdateWorkerDetails.cs
+++ SocialCareCaseViewerApi.Tests/V1/IntegrationTests/UpdateWorkerDetails.cs
@@ -9,6 +9,7 @@ using Newtonsoft.Json;
 using NUnit.Framework;
 using SocialCareCaseViewerApi.V1.Boundary.Requests;
 using SocialCareCaseViewerApi.V1.Boundary.Response;
+using SocialCareCaseViewerApi.V1.Infrastructure;
 using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace SocialCareCaseViewerApi.Tests.V1.IntegrationTests
@@ -86,14 +87,18 @@ namespace SocialCareCaseViewerApi.Tests.V1.IntegrationTests
             var allocationResponse = await Client.PostAsync(createAllocationUri, requestContent).ConfigureAwait(true);
             allocationResponse.StatusCode.Should().Be(201);
 
-            // Create another allocation request for existingDbWorker
-            var secondAllocationRequest = IntegrationTestHelpers.CreateAllocationRequest(_resident.Id, _existingDbTeam.Id, _existingDbWorker.Id, _allocationWorker);
-            var secondSerializedRequest = JsonSerializer.Serialize(secondAllocationRequest);
-
-            var secondRequestContent = new StringContent(secondSerializedRequest, Encoding.UTF8, "application/json");
-
-            var allocationTwoResponse = await Client.PostAsync(createAllocationUri, secondRequestContent).ConfigureAwait(true);
-            allocationTwoResponse.StatusCode.Should().Be(201);
+            // Create another allocation request for existingDbWorker, due to new restrictions in use case unable to create second allocation via API, saving directly to the DB.
+            var secondDBAllocation = new AllocationSet()
+            {
+                PersonId = _resident.Id,
+                TeamId = _existingDbTeam.Id,
+                WorkerId = _existingDbWorker.Id,
+                RagRating = "red",
+                CreatedBy = _allocationWorker.Email,
+                AllocationStartDate = DateTime.Now,
+            };
+            DatabaseContext.Allocations.Add(secondDBAllocation);
+            DatabaseContext.SaveChanges();
 
             // Patch request to update team of existingDbWorker
             var patchUri = new Uri("/api/v1/workers", UriKind.Relative);
