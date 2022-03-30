b46b7aef42f455b0a4b67d1c40dcb1a69fde905d
Juris Gribuska <juris.gribuska@hackney.gov.uk>
2022-03-30 11:55:32 +0100

    added interface

--- SocialCareCaseViewerApi/V1/Gateways/Interfaces/IDatabaseGateway.cs
+++ SocialCareCaseViewerApi/V1/Gateways/Interfaces/IDatabaseGateway.cs
@@ -39,5 +39,6 @@ namespace SocialCareCaseViewerApi.V1.Gateways.Interfaces
         void DeleteRelationship(long relationshipId);
         void CreateRequestAudit(CreateRequestAuditRequest request);
         void PatchPerson(PatchPersonRequest request);
+        UpdateAllocationResponse UpdateRagRatingInAllocation(UpdateAllocationRequest request);
     }
 }
