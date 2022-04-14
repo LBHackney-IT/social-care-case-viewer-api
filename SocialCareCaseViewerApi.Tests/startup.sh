34a89eeda84efc21542ef0e8a1cd680e063b58ec
LBHTKarki <tuomo.karki@hackney.gov.uk>
2022-04-14 14:46:22 +0100

    tweak sonarcloud setup

---
+++ SocialCareCaseViewerApi.Tests/startup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+dotnet sonarscanner begin /k:"LBHackney-IT_social-care-case-viewer-api" /o:"lbhackney-it" /d:sonar.host.url=https://sonarcloud.io /d:sonar.login="${SONAR_TOKEN}" /d:sonar.cs.vscoveragexml.reportsPaths=coverage.xml
+dotnet build -c debug -o out SocialCareCaseViewerApi.Tests/SocialCareCaseViewerApi.Tests.csproj --no-incremental
+dotnet-coverage collect 'dotnet test' -f xml -o 'coverage.xml'
+dotnet sonarscanner end /d:sonar.login="${SONAR_TOKEN}"
