602d3098908b9a7d5f4372b0203a5870e60751fa
LBHTKarki <tuomo.karki@hackney.gov.uk>
2022-04-14 15:08:40 +0100

    move dotnet commands to startup

--- SocialCareCaseViewerApi.Tests/startup.sh
+++ SocialCareCaseViewerApi.Tests/startup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+dotnet restore ./SocialCareCaseViewerApi/SocialCareCaseViewerApi.csproj
+dotnet restore ./SocialCareCaseViewerApi.Tests/SocialCareCaseViewerApi.Tests.csproj
+
 dotnet sonarscanner begin /k:"LBHackney-IT_social-care-case-viewer-api" /o:"lbhackney-it" /d:sonar.host.url=https://sonarcloud.io /d:sonar.login="${SONAR_TOKEN}" /d:sonar.cs.vscoveragexml.reportsPaths=coverage.xml
+
 dotnet build -c debug -o out SocialCareCaseViewerApi.Tests/SocialCareCaseViewerApi.Tests.csproj --no-incremental
 dotnet-coverage collect 'dotnet test' -f xml -o 'coverage.xml'
+
 dotnet sonarscanner end /d:sonar.login="${SONAR_TOKEN}"
