df0e9b0944153ae76dcce0df292bbb2d0f5f203b
Juris Gribuska <juris.gribuska@hackney.gov.uk>
2022-05-03 14:47:08 +0100

    added test and code for review date

--- SocialCareCaseViewerApi.Tests/V1/Gateways/Database/GetAllocationsSortingTests.cs
+++ SocialCareCaseViewerApi.Tests/V1/Gateways/Database/GetAllocationsSortingTests.cs
@@ -111,6 +111,48 @@ namespace SocialCareCaseViewerApi.Tests.V1.Gateways.Database
         }
 
         [Test]
+        public void GetAllocationsReturnResultsInPersonReviewDateOrderWithNullLastWhenRequested()
+        {
+            var worker = TestHelpers.CreateWorker(hasAllocations: false, hasWorkerTeams: false, id: 123);
+
+            var person1 = TestHelpers.CreatePerson(reviewDate: DateTime.Today.AddDays(10));
+            var allocation1 = TestHelpers.CreateAllocation(personId: (int) person1.Id, workerId: worker.Id, caseStatus: "open");
+
+            var person2 = TestHelpers.CreatePerson(reviewDate: DateTime.Today.AddDays(20));
+            var allocation2 = TestHelpers.CreateAllocation(personId: (int) person2.Id, workerId: worker.Id, caseStatus: "open");
+
+            var person3 = TestHelpers.CreatePerson(reviewDate: DateTime.Today.AddDays(30));
+            var allocation3 = TestHelpers.CreateAllocation(personId: (int) person3.Id, workerId: worker.Id, caseStatus: "open");
+
+            var person4 = TestHelpers.CreatePerson(reviewDate: DateTime.Today.AddDays(40));
+            var allocation4 = TestHelpers.CreateAllocation(personId: (int) person4.Id, workerId: worker.Id, caseStatus: "open");
+
+            var person5 = TestHelpers.CreatePerson();
+            person5.ReviewDate = null;
+            var allocation5 = TestHelpers.CreateAllocation(personId: (int) person5.Id, workerId: worker.Id, caseStatus: "open");
+
+            var person6 = TestHelpers.CreatePerson(reviewDate: null);
+            person6.ReviewDate = null;
+            var allocation6 = TestHelpers.CreateAllocation(personId: (int) person6.Id, workerId: worker.Id, caseStatus: "open");
+
+            DatabaseContext.Persons.AddRange(new List<Person> { person1, person2, person3, person4, person5, person6 });
+            DatabaseContext.Workers.Add(worker);
+            DatabaseContext.SaveChanges();
+
+            DatabaseContext.Allocations.AddRange(new List<AllocationSet>() { allocation1, allocation2, allocation3, allocation4, allocation5, allocation6 });
+            DatabaseContext.SaveChanges();
+
+            var (result, _, _) = _databaseGateway.SelectAllocations(0, workerId: worker.Id, workerEmail: "", 0, 0, "review_date");
+
+            result[0].Id.Should().Be(allocation1.Id);
+            result[1].Id.Should().Be(allocation2.Id);
+            result[2].Id.Should().Be(allocation3.Id);
+            result[3].Id.Should().Be(allocation4.Id);
+            result[4].Id.Should().BeOneOf(allocation5.Id, allocation6.Id);
+            result[5].Id.Should().BeOneOf(allocation5.Id, allocation6.Id);
+        }
+
+        [Test]
         public void GetAllocationsReturnResultsInPersonReviewDateOrderWhenRequested()
         {
             var worker = TestHelpers.CreateWorker(hasAllocations: false, hasWorkerTeams: false, id: 123);
