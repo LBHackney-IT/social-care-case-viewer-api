d7acbf06b0e5b0c1e824e982d7a4c64470bb04dd
Juris Gribuska <juris.gribuska@hackney.gov.uk>
2022-05-03 11:36:23 +0100

    total count in the interface and code

--- SocialCareCaseViewerApi/V1/UseCase/AllocationsUseCase.cs
+++ SocialCareCaseViewerApi/V1/UseCase/AllocationsUseCase.cs
@@ -14,12 +14,13 @@ namespace SocialCareCaseViewerApi.V1.UseCase
         }
         public AllocationList Execute(ListAllocationsRequest request)
         {
-            var (allocations, cursor) = _databaseGateway.SelectAllocations(request.MosaicId, request.WorkerId, request.WorkerEmail, request.TeamId, request.AllocationId, request.SortBy, request.Cursor, request.TeamAllocationStatus, request.Status);
+            var (allocations, cursor, totalCount) = _databaseGateway.SelectAllocations(request.MosaicId, request.WorkerId, request.WorkerEmail, request.TeamId, request.AllocationId, request.SortBy, request.Cursor, request.TeamAllocationStatus, request.Status);
 
             return new AllocationList
             {
                 Allocations = allocations,
-                NextCursor = cursor.ToString()
+                NextCursor = cursor.ToString(),
+                TotalCount = totalCount
             };
         }
 
