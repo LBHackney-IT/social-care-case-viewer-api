3e0f11d127608d462e8c3a52d8a478594fed896f
Juris Gribuska <juris.gribuska@hackney.gov.uk>
2022-05-05 14:30:16 +0100

    spacing

--- SocialCareCaseViewerApi.Tests/V1/Gateways/Database/GetTeamListTests.cs
+++ SocialCareCaseViewerApi.Tests/V1/Gateways/Database/GetTeamListTests.cs
@@ -4,9 +4,6 @@ using NUnit.Framework;
 using SocialCareCaseViewerApi.V1.Gateways;
 using SocialCareCaseViewerApi.Tests.V1.Helpers;
 using SocialCareCaseViewerApi.V1.Infrastructure;
-using Allocation = SocialCareCaseViewerApi.V1.Infrastructure.AllocationSet;
-using dbAddress = SocialCareCaseViewerApi.V1.Infrastructure.Address;
-using PhoneNumberInfrastructure = SocialCareCaseViewerApi.V1.Infrastructure.PhoneNumber;
 
 
 namespace SocialCareCaseViewerApi.Tests.V1.Gateways.Database
@@ -31,6 +28,7 @@ namespace SocialCareCaseViewerApi.Tests.V1.Gateways.Database
                 TestHelpers.CreateTeam(context: "A", name: "C"),
                 TestHelpers.CreateTeam(context: "A", name: "A")
             });
+
             DatabaseContext.SaveChanges();
             var teams = _teamGateway.GetTeamsByTeamContextFlag("A");
 
