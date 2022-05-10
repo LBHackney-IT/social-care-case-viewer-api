c07e6bac6e86627dd83532426ee338d495b881b2
Juris Gribuska <juris.gribuska@hackney.gov.uk>
2022-05-10 15:24:38 +0100

    more smells tweks

--- AlertsHandlerLambda/CustomExceptions.cs
+++ AlertsHandlerLambda/CustomExceptions.cs
@@ -6,12 +6,16 @@ namespace AlertsHandlerLambda
     [Serializable]
     public class GoogleApiException : Exception
     {
+        protected GoogleApiException(SerializationInfo info, StreamingContext context){}
+
         public GoogleApiException(SerializationInfo info, StreamingContext? context, string message) : base(message) { }
     }
 
     [Serializable]
     public class ConfigurationException : Exception
     {
+        protected ConfigurationException(SerializationInfo info, StreamingContext context){}
+
         public ConfigurationException(SerializationInfo info, StreamingContext? context, string message) : base(message) { }
     }
 }
