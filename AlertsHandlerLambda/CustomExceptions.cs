5cb503e3cc4e74714ccc76b6a6cd50e58e5e73cb
Juris Gribuska <juris.gribuska@hackney.gov.uk>
2022-05-10 14:15:21 +0100

    [Serializable] in GoogleApiException

--- AlertsHandlerLambda/CustomExceptions.cs
+++ AlertsHandlerLambda/CustomExceptions.cs
@@ -2,11 +2,13 @@ using System;
 
 namespace AlertsHandlerLambda
 {
+    [Serializable]
     public class GoogleApiException : Exception
     {
         public GoogleApiException(string message) : base(message) { }
     }
 
+    [Serializable]
     public class ConfigurationException : Exception
     {
         public ConfigurationException(string message) : base(message) { }
