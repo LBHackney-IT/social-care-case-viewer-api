1944bcca252e398c61019501dbd09cdbb92c3063
Juris Gribuska <juris.gribuska@hackney.gov.uk>
2022-05-10 10:00:38 +0100

    nullable datetime fix

--- AlertsHandlerLambda/CustomExceptions.cs
+++ AlertsHandlerLambda/CustomExceptions.cs
@@ -3,13 +3,11 @@ using System.Runtime.Serialization;
 
 namespace AlertsHandlerLambda
 {
-    [Serializable]
     public class GoogleApiException : Exception
     {
-        public GoogleApiException(SerializationInfo info, StreamingContext context, String message) : base(message) { }
+        public GoogleApiException(string message) : base(message) { }
     }
 
-    [Serializable]
     public class ConfigurationException : Exception
     {
         public ConfigurationException(string message) : base(message) { }
