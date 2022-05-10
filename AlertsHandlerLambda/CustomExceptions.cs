64e047ec5cf4f89eacd053695b4c469c330297ee
Juris Gribuska <juris.gribuska@hackney.gov.uk>
2022-05-10 09:32:56 +0100

    added serializable attribute

--- AlertsHandlerLambda/CustomExceptions.cs
+++ AlertsHandlerLambda/CustomExceptions.cs
@@ -1,13 +1,15 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace AlertsHandlerLambda
 {
     [Serializable]
     public class GoogleApiException : Exception
     {
-        public GoogleApiException(string message) : base(message) { }
+        public GoogleApiException(SerializationInfo info, StreamingContext context, String message) : base(message) { }
     }
 
+    [Serializable]
     public class ConfigurationException : Exception
     {
         public ConfigurationException(string message) : base(message) { }
