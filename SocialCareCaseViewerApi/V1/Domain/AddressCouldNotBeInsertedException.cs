c351d253346c94401aa0b15bdd91e54b01d3526a
Juris Gribuska <juris.gribuska@hackney.gov.uk>
2022-05-11 11:48:42 +0100

    iSerializable for AddressCouldNotBeInsertedException

--- SocialCareCaseViewerApi/V1/Domain/AddressCouldNotBeInsertedException.cs
+++ SocialCareCaseViewerApi/V1/Domain/AddressCouldNotBeInsertedException.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
 namespace SocialCareCaseViewerApi.V1.Domain
 {
+    [Serializable]
     public class AddressCouldNotBeInsertedException : Exception
     {
-        public AddressCouldNotBeInsertedException(string message) : base(message) { }
+        protected AddressCouldNotBeInsertedException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+        public AddressCouldNotBeInsertedException(SerializationInfo? info, StreamingContext? context, string message) : base(message) { }
     }
 }
